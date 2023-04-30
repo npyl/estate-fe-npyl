647ac4b4ecb99a4b8b746dc12913ff7bd9571cd5
Pete Marakos <petemarakos@gmail.com>
2023-04-30 15:02:55 +0000

    Npyl

---
+++ src/pages/property/[propertyId]/components/sections/index.ts
@@ -0,0 +1,9 @@
+export { default as AreaSection } from "./AreaSection";
+export { default as BalconiesSection } from "./BalconiesSection";
+export { default as BasicSection } from "./BasicSection";
+export { default as DetailsSection } from "./DetailsSection";
+export { default as DistanceSection } from "./DistanceSection";
+export { default as BlueprintsSection } from "./BlueprintsSection";
+export { default as HeatingSection } from "./HeatingSection";
+export { default as ImageSection } from "./ImageSection";
+export { default as ParkingsSection } from "./ParkingsSection";
