d6b1bd7aff24272c85932a845591dd984312ba38
taxiarchiszaronis <taxiarchiszaronis@digipath.gr>
2023-05-17 13:38:46 +0300

    ViewCustomer: grid, types and service

--- src/types/customer.ts
+++ src/types/customer.ts
@@ -1,18 +1,57 @@
 import { ILocation } from "./location";
 import { INote } from "./note";
+import { ILabel } from "./label";
 import { IProperties } from "./properties";
 import { IUser } from "./user";
+import { IPropertyFeatures } from "./features";
+
+interface IDemandFilters {
+  minBedrooms: number;
+  maxBedrooms: number;
+  minBathrooms: number;
+  maxBathrooms: number;
+  furnished: string;
+  maxCovered: number;
+  minCovered: number;
+  minPlot: number;
+  maxPlot: number;
+  minYearOfConstruction: number;
+  maxYearOfConstruction: number;
+  minFloor: string;
+  maxFloor: string;
+  parentCategory: string;
+  state: string;
+  minPrice: number;
+  maxPrice: number;
+}
+
+interface IDemand {
+  filters: IDemandFilters;
+  priorityFeatures: IPropertyFeatures;
+  nonPriorityFeatures: IPropertyFeatures;
+  timeframe: string;
+}
 
 export interface ICustomer {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    mobilePhone: string;
-    homePhone: string;
-    managedBy: IUser;
-    status: number;
-    location: ILocation;
-    notes: INote[];
-    properties: IProperties[];
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobilePhone: string;
+  homePhone: string;
+  managedBy: IUser;
+  status: number;
+  fax: string;
+  nationality: string;
+  idNumber: string;
+  passportNumber: string;
+  dateOfBirth: string;
+  leadSource: string;
+  preferredLanguage: string;
+  suggestedBy: string;
+  location: ILocation;
+  notes: INote[];
+  ownedProperties: IProperties[];
+  labels: ILabel[];
+  demand: IDemand;
 }
