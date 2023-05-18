51cb8a4cef71492953e641157e34b91daf004de0
npyl <npylarinos@digipath.gr>
2023-05-18 09:53:57 +0300

    Adding (not creating) Note component

--- src/pages/customer/create/components/NotesSection.tsx
+++ src/pages/customer/create/components/NotesSection.tsx
@@ -1,29 +1,10 @@
-import {
-  Grid,
-  Paper,
-  Stack,
-  InputBase,
-  IconButton,
-  Box,
-  Typography,
-  InputAdornment,
-} from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
-import { alpha } from "@mui/material/styles";
-
+import { Paper, Box, Typography } from "@mui/material";
 import * as React from "react";
 
-import { useRef, useState } from "react";
+import AddNote from "src/components/AddNote";
 
 const NotesSection: React.FC<any> = (props) => {
-  const commentInputRef = useRef<HTMLInputElement>(null);
-  const [message, setMessage] = useState("");
-
-  const handleChangeMessage = (value: string) => {
-    setMessage(value);
-  };
-
-  const handleAddNote = () => {};
+  const handleAddNote = (message: string) => {};
 
   return (
     <Paper
@@ -44,40 +25,11 @@ const NotesSection: React.FC<any> = (props) => {
         <Typography variant="h6">Notes</Typography>
       </Box>
 
-      <Grid item xs={12} padding={1}>
-        <Grid container spacing={2}>
-          <Stack
-            spacing={2}
-            direction="row"
-            alignItems="center"
-            sx={{
-              p: (theme) => theme.spacing(0, 3, 3, 3),
-            }}
-          >
-            <InputBase
-              fullWidth
-              value={message}
-              inputRef={commentInputRef}
-              placeholder="Write a comment…"
-              onChange={(event) => handleChangeMessage(event.target.value)}
-              endAdornment={
-                <InputAdornment position="end" sx={{ mr: 1 }}>
-                  <IconButton size="small" onClick={handleAddNote}>
-                    <SendIcon />
-                  </IconButton>
-                </InputAdornment>
-              }
-              sx={{
-                pl: 1.5,
-                height: 40,
-                borderRadius: 1,
-                border: (theme) =>
-                  `solid 1px ${alpha(theme.palette.grey[500], 0.32)}`,
-              }}
-            />
-          </Stack>
-        </Grid>
-      </Grid>
+      <AddNote
+        onAdd={(message) => {
+          handleAddNote(message);
+        }}
+      />
     </Paper>
   );
 };
