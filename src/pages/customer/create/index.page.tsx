61211a75ba56e327bc706071ddade9228dc32b3d
npyl <npylarinos@digipath.gr>
2023-05-24 12:28:43 +0300

    CreateCustomer: Implement adding notes

--- src/pages/customer/create/index.page.tsx
+++ src/pages/customer/create/index.page.tsx
@@ -4,7 +4,7 @@ import { DashboardLayout } from "src/components/dashboard/dashboard-layout";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 
-import { selectAll } from "src/slices/customer";
+import { selectAll, selectNotes } from "src/slices/customer";
 import { selectAll as selectAllNewLabels } from "src/slices/labels";
 
 import { useAddCustomerMutation } from "src/services/customers";
@@ -12,16 +12,19 @@ import { useCreateLabelForCustomerMutation } from "src/services/labels";
 
 import { NextPage } from "next";
 import Form from "../components/Form";
+import { useAddNoteToCustomerWithIdMutation } from "src/services/note";
 
 const CreateCustomer: NextPage = () => {
   const router = useRouter();
 
-  const [create, { isSuccess, data: createdCustomer }] =
+  const [create, { isSuccess: isCreateCustomerSuccess, data: createdCustomer }] =
     useAddCustomerMutation();
-
   const [createLabel, { isSuccess: isLabelSuccess }] =
     useCreateLabelForCustomerMutation();
+  const [createNote, { isSuccess: isNoteSuccess }] = useAddNoteToCustomerWithIdMutation();
+
   const newLabels = useSelector(selectAllNewLabels);
+  const newNotes = useSelector(selectNotes);
   const body = useSelector(selectAll);
 
   const createAndAssignNewLabels = () => {
@@ -34,15 +37,26 @@ const CreateCustomer: NextPage = () => {
         labelBody: newLabel,
       });
     });
-
-    router.push("/customer");
   };
-  isSuccess && createdCustomer && createAndAssignNewLabels();
+  const createAndAssignNewNotes = () => {
+    const createdCustomerId = createdCustomer!.id;
+
+    // foreach note; call create-for-customer-with-id
+    newNotes.forEach((newNote) => {
+      createNote({ id: createdCustomerId, dataToSend: { content: newNote.content } })
+    })
+  }
+
   const performUpload = () => {
     create(body);
+
+    isCreateCustomerSuccess && createdCustomer && createAndAssignNewLabels(); // create&assign labels
+    isCreateCustomerSuccess && createdCustomer && createAndAssignNewNotes();  // create&assign notes
+    isCreateCustomerSuccess && router.push("/customer");
+
   };
 
-  return <Form edit={false} performUpload={performUpload} customerId={""} />;
+  return <Form edit={false} performUpload={performUpload} />;
 };
 
 CreateCustomer.getLayout = (page) => (
