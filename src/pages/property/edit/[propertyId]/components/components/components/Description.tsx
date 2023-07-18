561c90e973b56f24ba9e4b5b8b187b738ef9c9d5
npyl <npylarinos@digipath.gr>
2023-07-18 17:08:22 +0300

    build fix

--- src/pages/property/edit/[propertyId]/components/components/components/Description.tsx
+++ src/pages/property/edit/[propertyId]/components/components/components/Description.tsx
@@ -2,44 +2,44 @@ import { connect } from "react-redux";
 import { setDescription } from "src/slices/property";
 import { Box, Card, CardContent, CardHeader, Grid } from "@mui/material";
 import * as React from "react";
-import Editor from "src/components/editor/Editor";
+// import Editor from "src/components/editor/Editor";
 
 interface DescriptionSectionProps {
-  description: string;
-  setDescription: (description: string) => void;
+	description: string;
+	setDescription: (description: string) => void;
 }
 
 const DescriptionSection: React.FC<DescriptionSectionProps> = ({
-  description,
-  setDescription,
+	description,
+	setDescription,
 }) => {
-  return (
-    <Grid item xs={12} padding={0}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Card>
-            <CardHeader title="Description" />
-            <CardContent>
-              <Editor
+	return (
+		<Grid item xs={12} padding={0}>
+			<Grid container spacing={2}>
+				<Grid item xs={12}>
+					<Card>
+						<CardHeader title="Description" />
+						<CardContent>
+							{/* <Editor
                 id="full-editor"
                 value={description}
                 onChange={(value) => setDescription(value)}
-              />
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </Grid>
-  );
+              /> */}
+						</CardContent>
+					</Card>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 };
 
 const mapStateToProps = (state: any) => ({
-  description: state.property.description,
+	description: state.property.description,
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-  setDescription: (description: string) =>
-    dispatch(setDescription(description)),
+	setDescription: (description: string) =>
+		dispatch(setDescription(description)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DescriptionSection);
