115dd97a4247990761a516c4c0441c3df1f6d13c
npyl <npylarinos@digipath.gr>
2023-07-19 09:39:01 +0300

    Bring back Editor: EditorToolbar needs to be a server-side-disabled import because it is using document.() calls

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -4,105 +4,107 @@ import dynamic from "next/dynamic";
 // @mui
 import { Box } from "@mui/material";
 //
-import EditorToolbar from "./EditorToolbar";
-
 import { StyledEditor } from "./styles";
 import { EditorProps } from "./types";
 
 const ReactQuill = dynamic(() => import("react-quill"), {
-  ssr: false,
-  loading: () => (
-    <Box
-      sx={{
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        position: "absolute",
-        bgcolor: "background.paper",
-      }}
-    >
-      Loading...
-    </Box>
-  ),
+	ssr: false,
+	loading: () => (
+		<Box
+			sx={{
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+				position: "absolute",
+				bgcolor: "background.paper",
+			}}
+		>
+			Loading...
+		</Box>
+	),
+});
+
+const EditorToolbar = dynamic(() => import("./EditorToolbar"), {
+	ssr: false,
 });
 
 export const formats = [
-  "align",
-  "background",
-  "blockquote",
-  "bold",
-  "bullet",
-  "code",
-  "code-block",
-  "color",
-  "direction",
-  "font",
-  "formula",
-  "header",
-  "image",
-  "indent",
-  "italic",
-  "link",
-  "list",
-  "script",
-  "size",
-  "strike",
-  "table",
-  "underline",
-  "emoji",
+	"align",
+	"background",
+	"blockquote",
+	"bold",
+	"bullet",
+	"code",
+	"code-block",
+	"color",
+	"direction",
+	"font",
+	"formula",
+	"header",
+	"image",
+	"indent",
+	"italic",
+	"link",
+	"list",
+	"script",
+	"size",
+	"strike",
+	"table",
+	"underline",
+	"emoji",
 ];
 
 // ----------------------------------------------------------------------
 
 export default function Editor({
-  id = "minimal-quill",
-  error,
-  value,
-  onChange,
-  simple = false,
-  helperText,
-  sx,
-  ...other
+	id = "minimal-quill",
+	error,
+	value,
+	onChange,
+	simple = false,
+	helperText,
+	sx,
+	...other
 }: EditorProps) {
-  const modules = {
-    toolbar: {
-      container: `#${id}`,
-    },
-    history: {
-      delay: 500,
-      maxStack: 300,
-      userOnly: true,
-    },
-    syntax: true,
-    clipboard: {
-      matchVisual: false,
-    },
-  };
+	const modules = {
+		toolbar: {
+			container: `#${id}`,
+		},
+		history: {
+			delay: 500,
+			maxStack: 300,
+			userOnly: true,
+		},
+		syntax: true,
+		clipboard: {
+			matchVisual: false,
+		},
+	};
 
-  return (
-    <>
-      <StyledEditor
-        sx={{
-          ...(error && {
-            border: (theme) => `solid 1px ${theme.palette.error.main}`,
-          }),
-          ...sx,
-        }}
-      >
-        <EditorToolbar id={id} isSimple={simple} />
+	return (
+		<>
+			<StyledEditor
+				sx={{
+					...(error && {
+						border: (theme) => `solid 1px ${theme.palette.error.main}`,
+					}),
+					...sx,
+				}}
+			>
+				<EditorToolbar id={id} isSimple={simple} />
 
-        <ReactQuill
-          value={value}
-          onChange={onChange}
-          modules={modules}
-          formats={formats}
-          placeholder="Write your Description"
-          {...other}
-        />
-      </StyledEditor>
+				<ReactQuill
+					value={value}
+					onChange={onChange}
+					modules={modules}
+					formats={formats}
+					placeholder="Write your Description"
+					{...other}
+				/>
+			</StyledEditor>
 
-      {helperText && helperText}
-    </>
-  );
+			{helperText && helperText}
+		</>
+	);
 }
