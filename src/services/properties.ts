da343f7a33a982d57eaaeb1aa3c9703579962732
npyl <npylarinos@digipath.gr>
2023-07-25 09:42:37 +0300

    RE-153

--- src/services/properties.ts
+++ src/services/properties.ts
@@ -105,7 +105,7 @@ export const properties = createApi({
 				method: "POST",
 				body: props.body,
 			}),
-			invalidatesTags: ["Properties"],
+			invalidatesTags: ["Properties", "PropertyById"],
 		}),
 		createProperty: builder.mutation<number, ICreatePropertyParams>({
 			query: (dataToSend: ICreatePropertyParams) => ({
