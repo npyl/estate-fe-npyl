06e49d28a0a06419db02455ddc4e8e153674f94c
npyl <npylarinos@digipath.gr>
2023-07-25 15:40:22 +0300

    RE-28

--- src/slices/filters.ts
+++ src/slices/filters.ts
@@ -3,246 +3,256 @@ import { RootState } from "src/store";
 import { IPropertyFilter } from "src/types/properties";
 
 interface IFilterProps extends IPropertyFilter {
-  [key: string]: any;
+	[key: string]: any;
 }
 
 const initialState: IFilterProps = {
-  parentCategories: [],
-  categories: [],
-  labels: [],
-  states: [],
-  cities: [],
-  points: [],
+	parentCategories: [],
+	categories: [],
+	labels: [],
+	states: [],
+	cities: [],
+	points: [],
+	frameType: [],
+	furnished: [],
+	heatingType: [],
 };
 
 const slice = createSlice({
-  name: "filters",
-  initialState,
-  reducers: {
-    setCode(state, { payload }) {
-      state.code = payload;
-    },
+	name: "filters",
+	initialState,
+	reducers: {
+		setCode(state, { payload }) {
+			state.code = payload;
+		},
 
-    setFrameType(state, { payload }) {
-      state.frameType = payload;
-    },
+		setManagerId(state, { payload }) {
+			state.managerId = payload;
+		},
 
-    setFurnished(state, { payload }) {
-      state.furnished = payload;
-    },
+		setMaxArea(state, { payload }) {
+			state.maxArea = payload;
+		},
 
-    setHeatingType(state, { payload }) {
-      state.heatingType = payload;
-    },
+		setMaxBedrooms(state, { payload }) {
+			state.maxBedrooms = payload;
+		},
 
-    setManagerId(state, { payload }) {
-      state.managerId = payload;
-    },
+		setMaxConstructionYear(state, { payload }) {
+			state.maxConstructionYear = payload;
+		},
 
-    setMaxArea(state, { payload }) {
-      state.maxArea = payload;
-    },
+		setMaxFloor(state, { payload }) {
+			state.maxFloor = payload;
+		},
 
-    setMaxBedrooms(state, { payload }) {
-      state.maxBedrooms = payload;
-    },
+		setMaxPrice(state, { payload }) {
+			state.maxPrice = payload;
+		},
 
-    setMaxConstructionYear(state, { payload }) {
-      state.maxConstructionYear = payload;
-    },
+		setMinArea(state, { payload }) {
+			state.minArea = payload;
+		},
 
-    setMaxFloor(state, { payload }) {
-      state.maxFloor = payload;
-    },
+		setMinBedrooms(state, { payload }) {
+			state.minBedrooms = payload;
+		},
 
-    setMaxPrice(state, { payload }) {
-      state.maxPrice = payload;
-    },
+		setMinConstructionYear(state, { payload }) {
+			state.minConstructionYear = payload;
+		},
 
-    setMinArea(state, { payload }) {
-      state.minArea = payload;
-    },
+		setMinFloor(state, { payload }) {
+			state.minFloor = payload;
+		},
 
-    setMinBedrooms(state, { payload }) {
-      state.minBedrooms = payload;
-    },
+		setMinPrice(state, { payload }) {
+			state.minPrice = payload;
+		},
 
-    setMinConstructionYear(state, { payload }) {
-      state.minConstructionYear = payload;
-    },
+		// multiple
+		setLabels(state, { payload }) {
+			state.labels = payload;
+		},
+		setParentLocation(state, { payload }) {
+			state.parentLocation = payload;
+		},
+		setSubLocation(state, { payload }) {
+			state.subLocation = payload;
+		},
+		setStates(state, { payload }) {
+			state.states = payload;
+		},
+		setSubCategories(state, { payload }) {
+			state.categories = payload;
+		},
+		setParentCategories(state, { payload }) {
+			state.parentCategories = payload;
+		},
+		deleteSubCategory(state, { payload }) {
+			state.categories = state.categories.filter(
+				(category) => category !== payload
+			);
+		},
 
-    setMinFloor(state, { payload }) {
-      state.minFloor = payload;
-    },
+		toggleFrameType(state, { payload }) {
+			state.frameType = state.frameType.includes(payload)
+				? state.frameType.filter((o) => payload !== o) // already exists; remove
+				: [...state.frameType, payload]; // doesn't exist; add
+		},
 
-    setMinPrice(state, { payload }) {
-      state.minPrice = payload;
-    },
+		toggleFurnished(state, { payload }) {
+			state.furnished = state.furnished.includes(payload)
+				? state.furnished.filter((o) => payload !== o) // already exists; remove
+				: [...state.furnished, payload]; // doesn't exist; add
+		},
 
-    // multiple
-    setLabels(state, { payload }) {
-      state.labels = payload;
-    },
-    setParentLocation(state, { payload }) {
-      state.parentLocation = payload;
-    },
-    setSubLocation(state, { payload }) {
-      state.subLocation = payload;
-    },
-    setStates(state, { payload }) {
-      state.states = payload;
-    },
-    setSubCategories(state, { payload }) {
-      state.categories = payload;
-    },
-    setParentCategories(state, { payload }) {
-      state.parentCategories = payload;
-    },
-    deleteSubCategory(state, { payload }) {
-      state.categories = state.categories.filter(
-        (category) => category !== payload
-      );
-    },
-    deleteState(state, { payload }) {
-      state.states = state.states.filter((state) => state !== payload);
-    },
+		toggleHeatingType(state, { payload }) {
+			state.heatingType = state.heatingType.includes(payload)
+				? state.heatingType.filter((o) => payload !== o) // already exists; remove
+				: [...state.heatingType, payload]; // doesn't exist; add
+		},
 
-    deleteFilter(state, { payload }) {
-      const key = payload;
-      const initialValue = initialState[payload];
-      state[key] = initialValue;
-    },
+		deleteState(state, { payload }) {
+			state.states = state.states.filter((state) => state !== payload);
+		},
 
-    resetState: () => {
-      return initialState;
-    },
-  },
+		deleteFilter(state, { payload }) {
+			const key = payload;
+			const initialValue = initialState[payload];
+			state[key] = initialValue;
+		},
+
+		resetState: () => {
+			return initialState;
+		},
+	},
 });
 
 export const {
-  setCode,
-  setFrameType,
-  setFurnished,
-  setHeatingType,
-  setManagerId,
-  setMaxArea,
-  setMaxBedrooms,
-  setMaxConstructionYear,
-  setMaxFloor,
-  setMaxPrice,
-  setMinArea,
-  setMinBedrooms,
-  setMinConstructionYear,
-  setMinFloor,
-  setMinPrice,
+	setCode,
+	toggleFrameType,
+	toggleFurnished,
+	toggleHeatingType,
+	setManagerId,
+	setMaxArea,
+	setMaxBedrooms,
+	setMaxConstructionYear,
+	setMaxFloor,
+	setMaxPrice,
+	setMinArea,
+	setMinBedrooms,
+	setMinConstructionYear,
+	setMinFloor,
+	setMinPrice,
 
-  // multiple
-  setLabels,
-  setParentLocation,
-  setSubLocation,
-  setStates,
-  setSubCategories,
-  setParentCategories,
+	// multiple
+	setLabels,
+	setParentLocation,
+	setSubLocation,
+	setStates,
+	setSubCategories,
+	setParentCategories,
 
-  deleteSubCategory,
-  deleteState,
+	deleteSubCategory,
+	deleteState,
 
-  deleteFilter,
-  resetState,
+	deleteFilter,
+	resetState,
 } = slice.actions;
 
 export const selectCode = ({ filters }: RootState) => filters.code;
 export const selectFrameType = ({ filters }: RootState) => filters.frameType;
 export const selectFurnished = ({ filters }: RootState) => filters.furnished;
 export const selectHeatingType = ({ filters }: RootState) =>
-  filters.heatingType;
+	filters.heatingType;
 export const selectManagerId = ({ filters }: RootState) => filters.managerId;
 export const selectMaxArea = ({ filters }: RootState) => filters.maxArea;
 export const selectMaxBedrooms = ({ filters }: RootState) =>
-  filters.maxBedrooms;
+	filters.maxBedrooms;
 export const selectMaxConstructionYear = ({ filters }: RootState) =>
-  filters.maxConstructionYear;
+	filters.maxConstructionYear;
 export const selectMaxFloor = ({ filters }: RootState) => filters.maxFloor;
 export const selectMaxPrice = ({ filters }: RootState) => filters.maxPrice;
 export const selectMinArea = ({ filters }: RootState) => filters.minArea;
 export const selectMinBedrooms = ({ filters }: RootState) =>
-  filters.minBedrooms;
+	filters.minBedrooms;
 export const selectMinConstructionYear = ({ filters }: RootState) =>
-  filters.minConstructionYear;
+	filters.minConstructionYear;
 export const selectMinFloor = ({ filters }: RootState) => filters.minFloor;
 export const selectMinPrice = ({ filters }: RootState) => filters.minPrice;
 export const selectParentLocation = ({ filters }: RootState) =>
-  filters.parentLocation;
+	filters.parentLocation;
 export const selectStates = ({ filters }: RootState) => filters.states;
 export const selectParentCategories = ({ filters }: RootState) =>
-  filters.parentCategories;
+	filters.parentCategories;
 export const selectSubCategories = ({ filters }: RootState) =>
-  filters.categories;
+	filters.categories;
 export const selectLabels = ({ filters }: RootState) => filters.labels;
 
 export const selectAll = ({ filters }: RootState) => filters;
 
 const sumOfChangedProperties = createSelector(
-  (state: RootState) => state.filters,
-  (filter) => {
-    const propertiesToInclude = [
-      "filterName",
-      "code",
-      "minPrice",
-      "maxPrice",
-      "minArea",
-      "maxArea",
-      "minBedrooms",
-      "maxBedrooms",
-      "minFloor",
-      "maxFloor",
-      "minConstructionYear",
-      "maxConstructionYear",
-      "heatingType",
-      "frameType",
-      "furnished",
-      "managerId",
+	(state: RootState) => state.filters,
+	(filter) => {
+		const propertiesToInclude = [
+			"filterName",
+			"code",
+			"minPrice",
+			"maxPrice",
+			"minArea",
+			"maxArea",
+			"minBedrooms",
+			"maxBedrooms",
+			"minFloor",
+			"maxFloor",
+			"minConstructionYear",
+			"maxConstructionYear",
+			"managerId",
 
-      // multiple
+			// multiple
 
-      "states",
-      "parentCategories",
-      "categories",
-      "labels",
-    ];
+			"states",
+			"parentCategories",
+			"categories",
+			"labels",
+			"heatingType",
+			"frameType",
+			"furnished",
+		];
 
-    return propertiesToInclude.reduce(
-      (acc, curr) =>
-        filter[curr] !== initialState[curr] ? // ignore default filter values (e.g. yearOfConstruction = 1960)
-          Array.isArray(filter[curr])
-            ? filter[curr].length > 0
-              ? acc + 1
-              : acc
-            : filter[curr]
-              ? acc + 1
-              : acc
-          : acc,
-      0
-    );
-  }
+		return propertiesToInclude.reduce(
+			(acc, curr) =>
+				filter[curr] !== initialState[curr] // ignore default filter values (e.g. yearOfConstruction = 1960)
+					? Array.isArray(filter[curr])
+						? filter[curr].length > 0
+							? acc + 1
+							: acc
+						: filter[curr]
+						? acc + 1
+						: acc
+					: acc,
+			0
+		);
+	}
 );
 
 export default sumOfChangedProperties;
 
 export const getChangedFields = createSelector(
-  (state: RootState) => state.filters,
-  (filter) => {
-    const changedFields = Object.entries(filter).reduce(
-      (acc: any, [key, value]) => {
-        if (value !== initialState[key]) {
-          acc[key] = value;
-        }
-        return acc;
-      },
-      {}
-    );
-    return changedFields;
-  }
+	(state: RootState) => state.filters,
+	(filter) => {
+		const changedFields = Object.entries(filter).reduce(
+			(acc: any, [key, value]) => {
+				if (value !== initialState[key]) {
+					acc[key] = value;
+				}
+				return acc;
+			},
+			{}
+		);
+		return changedFields;
+	}
 );
 
 export const { reducer } = slice;
