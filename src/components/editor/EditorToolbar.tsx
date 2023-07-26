6624a2493bf35ad69613298d74e8f07a25543a3a
npyl <npylarinos@digipath.gr>
2023-07-26 11:18:38 +0300

    Fix toolbar errors (for now i guess)

--- src/components/editor/EditorToolbar.tsx
+++ src/components/editor/EditorToolbar.tsx
@@ -7,12 +7,12 @@ import { Quill } from "react-quill";
 // import styled from "styled-components";
 
 const HEADINGS = [
-  "Heading 1",
-  "Heading 2",
-  "Heading 3",
-  "Heading 4",
-  "Heading 5",
-  "Heading 6",
+	"Heading 1",
+	"Heading 2",
+	"Heading 3",
+	"Heading 4",
+	"Heading 5",
+	"Heading 6",
 ];
 
 // const CustomUndo = () => (
@@ -52,51 +52,35 @@ Quill.register(Size, true);
 // Add fonts to whitelist and register them
 const Font = Quill.import("formats/font");
 Font.whitelist = [
-  "arial",
-  "comic-sans",
-  "courier-new",
-  "georgia",
-  "helvetica",
-  "lucida",
+	"arial",
+	"comic-sans",
+	"courier-new",
+	"georgia",
+	"helvetica",
+	"lucida",
 ];
 Quill.register(Font, true);
 
-// Modules object for setting up the Quill editor
-export const modules = {
-  toolbar: {
-    container: "#toolbar",
-    // handlers: {
-    //   undo: undoChange,
-    //   redo: redoChange,
-    // },
-  },
-  history: {
-    delay: 500,
-    maxStack: 100,
-    userOnly: true,
-  },
-};
-
 // Formats objects for setting up the Quill editor
 export const formats = [
-  "header",
-  "font",
-  "size",
-  "bold",
-  "italic",
-  "underline",
-  "align",
-  "strike",
-  "script",
-  "blockquote",
-  "background",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "image",
-  "color",
-  "code-block",
+	"header",
+	"font",
+	"size",
+	"bold",
+	"italic",
+	"underline",
+	"align",
+	"strike",
+	"script",
+	"blockquote",
+	"background",
+	"list",
+	"bullet",
+	"indent",
+	"link",
+	"image",
+	"color",
+	"code-block",
 ];
 
 // export const formats = [
@@ -125,100 +109,100 @@ export const formats = [
 //   "emoji”,
 // ];
 type EditorToolbarProps = {
-  id: string;
-  isSimple?: boolean;
+	id: string;
+	isSimple?: boolean;
 };
 export default function EditorToolbar({
-  id,
-  isSimple,
-  ...other
+	id,
+	isSimple,
+	...other
 }: EditorToolbarProps) {
-  const [emoji, setEmoji] = useState(false);
-  const editorRef = useRef(null);
-  // const handleEmojiSelect = (selectedEmoji) => {
-  //   if (editorRef.current) {
-  //     const selection = window.getSelection();
-  //     if (!selection) return null;
-  //     const range = selection.getRangeAt(0);
-  //     const textNode = document.createTextNode(selectedEmoji.native);
-  //     range.insertNode(textNode);
-  //     if (!textNode) return null;
-  //     range.setStartAfter(textNode);
-  //     range.setEndAfter(textNode);
-  //     selection.removeAllRanges();
-  //     selection.addRange(range);
-  //   }
-  // };
-  return (
-    <StyledEditorToolbar ref={editorRef} {...other}>
-      <div id={id}>
-        <select className="ql-font" defaultValue="arial">
-          <option value="arial">Arial</option>
-          <option value="comic-sans">Comic Sans</option>
-          <option value="courier-new">Courier New</option>
-          <option value="georgia">Georgia</option>
-          <option value="helvetica">Helvetica</option>
-          <option value="lucida">Lucida</option>
-        </select>
-        <select className="ql-size" defaultValue="medium">
-          <option value="small">Size 1</option>
-          <option value="medium">Size 2</option>
-          <option value="large">Size 3</option>
-        </select>
-        <div className="ql-formats">
-          <select className="ql-header" defaultValue="">
-            {HEADINGS.map((heading, index) => (
-              <option key={heading} value={index + 1}>
-                {heading}
-              </option>
-            ))}
-            <option value="">Normal</option>
-          </select>
-        </div>
+	const [emoji, setEmoji] = useState(false);
+	const editorRef = useRef(null);
+	// const handleEmojiSelect = (selectedEmoji) => {
+	//   if (editorRef.current) {
+	//     const selection = window.getSelection();
+	//     if (!selection) return null;
+	//     const range = selection.getRangeAt(0);
+	//     const textNode = document.createTextNode(selectedEmoji.native);
+	//     range.insertNode(textNode);
+	//     if (!textNode) return null;
+	//     range.setStartAfter(textNode);
+	//     range.setEndAfter(textNode);
+	//     selection.removeAllRanges();
+	//     selection.addRange(range);
+	//   }
+	// };
+	return (
+		<StyledEditorToolbar ref={editorRef} {...other}>
+			<div id={id}>
+				<select className="ql-font" defaultValue="arial">
+					<option value="arial">Arial</option>
+					<option value="comic-sans">Comic Sans</option>
+					<option value="courier-new">Courier New</option>
+					<option value="georgia">Georgia</option>
+					<option value="helvetica">Helvetica</option>
+					<option value="lucida">Lucida</option>
+				</select>
+				<select className="ql-size" defaultValue="medium">
+					<option value="small">Size 1</option>
+					<option value="medium">Size 2</option>
+					<option value="large">Size 3</option>
+				</select>
+				<div className="ql-formats">
+					<select className="ql-header" defaultValue="">
+						{HEADINGS.map((heading, index) => (
+							<option key={heading} value={index + 1}>
+								{heading}
+							</option>
+						))}
+						<option value="">Normal</option>
+					</select>
+				</div>
 
-        <div className="ql-formats">
-          <button type="button" className="ql-bold" />
-          <button type="button" className="ql-italic" />
-          <button type="button" className="ql-underline" />
-          <button type="button" className="ql-strike" />
-        </div>
+				<div className="ql-formats">
+					<button type="button" className="ql-bold" />
+					<button type="button" className="ql-italic" />
+					<button type="button" className="ql-underline" />
+					<button type="button" className="ql-strike" />
+				</div>
 
-        {!isSimple && (
-          <div className="ql-formats">
-            <select className="ql-color" />
-            <select className="ql-background" />
-          </div>
-        )}
+				{!isSimple && (
+					<div className="ql-formats">
+						<select className="ql-color" />
+						<select className="ql-background" />
+					</div>
+				)}
 
-        <div className="ql-formats">
-          <button type="button" className="ql-list" value="ordered" />
-          <button type="button" className="ql-list" value="bullet" />
-          {!isSimple && (
-            <button type="button" className="ql-indent" value="-1" />
-          )}
-          {!isSimple && (
-            <button type="button" className="ql-indent" value="+1" />
-          )}
-        </div>
+				<div className="ql-formats">
+					<button type="button" className="ql-list" value="ordered" />
+					<button type="button" className="ql-list" value="bullet" />
+					{!isSimple && (
+						<button type="button" className="ql-indent" value="-1" />
+					)}
+					{!isSimple && (
+						<button type="button" className="ql-indent" value="+1" />
+					)}
+				</div>
 
-        {!isSimple && (
-          <div className="ql-formats">
-            <button type="button" className="ql-code-block" />
-            <button type="button" className="ql-blockquote" />
-          </div>
-        )}
+				{!isSimple && (
+					<div className="ql-formats">
+						<button type="button" className="ql-code-block" />
+						<button type="button" className="ql-blockquote" />
+					</div>
+				)}
 
-        <div className="ql-formats">
-          <button type="button" className="ql-direction" value="rtl" />
-          <select className="ql-align" />
-        </div>
+				<div className="ql-formats">
+					<button type="button" className="ql-direction" value="rtl" />
+					<select className="ql-align" />
+				</div>
 
-        <div className="ql-formats">
-          <button type="button" className="ql-link" />
-          <button type="button" className="ql-image" />
-          <button type="button" className="ql-video" />
-        </div>
-        {/* <span className="ql-formats">
+				<div className="ql-formats">
+					<button type="button" className="ql-link" />
+					<button type="button" className="ql-image" />
+					<button type="button" className="ql-video" />
+				</div>
+				{/* <span className="ql-formats">
           <button className="ql-undo">
             <CustomUndo />
           </button>
@@ -227,7 +211,7 @@ export default function EditorToolbar({
           </button>
         </span> */}
 
-        {/* <div className='ql-formats'>
+				{/* <div className='ql-formats'>
           <button
             type=‘button’
             className=‘ql-emoji’
@@ -237,7 +221,7 @@ export default function EditorToolbar({
           </button>
           {emoji && <Picker onEmojiSelect={handleEmojiSelect} />}
         </div> */}
-      </div>
-    </StyledEditorToolbar>
-  );
+			</div>
+		</StyledEditorToolbar>
+	);
 }
