305286bda01cf3809d26690f4616a50513dfa877
npyl <npylarinos@digipath.gr>
2023-08-03 11:38:44 +0300

    BulkEdit: RE-69:

--- src/pages/components/BulkEdit/Edit.tsx
+++ src/pages/components/BulkEdit/Edit.tsx
@@ -1,4 +1,8 @@
 import { Autocomplete, FormControl, TextField } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import OnlyNumbersInput from "src/components/OnlyNumbers";
+
+import { useAllCustomersQuery } from "src/services/customers";
 import { useAllUsersQuery } from "src/services/user";
 
 interface EditProps<T> {
@@ -38,15 +42,61 @@ export const EditManager = ({ data, setData }: EditProps<string>) => {
 };
 
 export const EditOwner = ({ data, setData }: EditProps<string>) => {
-    return <div>EditOwner Component</div>;
+    const owners: string[] =
+        useAllCustomersQuery(undefined, {
+            selectFromResult: ({ data }) => ({
+                data: data
+                    ?.filter(
+                        (customer) => customer.firstName && customer.lastName
+                    ) // filter nulls
+                    .map((customer) => {
+                        return customer.firstName + " " + customer.lastName;
+                    }),
+            }),
+        }).data || [];
+
+    const autocompleteChange = (_event: any, value: string | null) => {
+        setData(value || "");
+    };
+
+    return (
+        <FormControl fullWidth>
+            <Autocomplete
+                disablePortal
+                value={data}
+                onChange={autocompleteChange}
+                options={owners}
+                renderInput={(params) => (
+                    <TextField {...params} label="Owner" />
+                )}
+            />
+        </FormControl>
+    );
 };
 
 export const EditZipCode = ({ data, setData }: EditProps<string>) => {
-    return <div>EditZipCode Component</div>;
+    const { t } = useTranslation();
+
+    return (
+        <OnlyNumbersInput
+            label={t("Zip Code")}
+            value={data}
+            onChange={setData}
+        />
+    );
 };
 
 export const EditArea = ({ data, setData }: EditProps<string>) => {
-    return <div>EditArea Component</div>;
+    const { t } = useTranslation();
+
+    return (
+        <OnlyNumbersInput
+            label={t("Area")}
+            value={data}
+            onChange={setData}
+            adornment="m²"
+        />
+    );
 };
 
 export const EditLabels = ({ data, setData }: EditProps<number[]>) => {
@@ -54,7 +104,17 @@ export const EditLabels = ({ data, setData }: EditProps<number[]>) => {
 };
 
 export const EditBedrooms = ({ data, setData }: EditProps<string>) => {
-    return <div>EditBedrooms Component</div>;
+    const { t } = useTranslation();
+
+    return (
+        <OnlyNumbersInput
+            type="number"
+            label={t("Bedrooms")}
+            placeholder="1,2,3..."
+            value={data}
+            onChange={setData}
+        />
+    );
 };
 
 export const EditStatus = ({ data, setData }: EditProps<string>) => {
