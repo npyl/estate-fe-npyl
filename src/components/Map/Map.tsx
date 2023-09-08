af612b0000a943ec7388024844241a989785c77b
npyl <n.pylarinos@hotmail.com>
2023-09-08 11:46:02 +0300

    fixup

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -52,6 +52,14 @@ interface IMapProps {
 const apiKey = "AIzaSyCW6oijpbC0JhlXRwPBtNIxy9e4sn7NnwU";
 const athensLatLng = { lat: 37.98381, lng: 23.727539 };
 
+export const useLoadApi = () => {
+    return useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: apiKey,
+        libraries: ["drawing", "places", "geometry"],
+    });
+}
+
 const Map = ({
     onReady,
     onClick,
@@ -68,11 +76,8 @@ const Map = ({
     drawing = true,
     search = false,
 }: IMapProps) => {
-    const { isLoaded } = useJsApiLoader({
-        id: "google-map-script",
-        googleMapsApiKey: apiKey,
-        libraries: ["drawing", "places"],
-    });
+    const { isLoaded } = useLoadApi();
+
     const [geocoder, setGeocoder] = useState<google.maps.Geocoder>();
 
     const [map, setMap] = React.useState(null);
