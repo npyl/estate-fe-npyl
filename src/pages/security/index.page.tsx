dd7321f28469f9fd755d7f815119c2e05a998ba6
Leonidas.Panagiotou <leonidas.panagiotou@govegans.it>
2023-09-12 19:15:06 +0300

    2nd draft

--- src/pages/security/index.page.tsx
+++ src/pages/security/index.page.tsx
@@ -1,3 +1,20 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+import { useAllGlobalsQuery } from "src/services/global";
+
 import type { NextPage } from "next";
 import { useMemo } from "react";
 import { AuthGuard } from "src/components/authentication/auth-guard";
@@ -7,46 +24,139 @@ import {
     useGetNotificationByIdQuery,
     useGetNotificationsQuery,
 } from "src/services/notification";
-import { Paper } from "@mui/material";
+import {Checkbox, Paper, Stack} from "@mui/material";
 import { usePublishTab } from "src/components/Tabs/utils";
 import { ContactNotification } from "src/types/notification";
+import {width} from "@mui/system";
+
+function createTestData() {
+    return {
+        "parentCategories": [
+            "Residential",
+            "Commercial",
+            "Rural",
+            "Industrial",
+            "Unincorporated",
+            "Recreational",
+            "Unknown"
+        ],
+        "residentialCategories": [
+            "Apartment",
+            "Studio",
+            "Maisonette",
+            "Detached",
+            "Villa",
+            "Loft",
+            "Bungalow",
+            "Building",
+            "Apartment complex",
+            "Farm",
+            "Houseboat",
+            "Other residential"
+        ],
+        "states": [
+            "Sale",
+            "Rent",
+            "Sold",
+            "Rented",
+            "Unavailable",
+            "Taken",
+            "Under Construction",
+            "Under Maintenance"
+        ],
+        "actions": [
+            "allowDelete",
+            "allowEdit",
+            "accessOwner",
+            "accessLocation",
+            "accessPrice",
+            "accessActive",
+            "accessHidden",
+            "accessInactive"
+        ]
+    };
+}
+
+// Usage:
+function Row(props) {
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
+
+    const testData = createTestData();
+
+    return (
+        <React.Fragment >
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                <TableCell colSpan={9}>
+                    <Stack direction={'row'} alignItems={'center'}>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                    <Typography variant={'h6'}>{row} </Typography>
+                    </Stack>
+                </TableCell>
+            </TableRow>
+
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0, padding:0}} colSpan={testData.actions.length+1}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box>
+                            <Table size="small" aria-label="purchases">
+                                <TableBody>
+                                    {testData.residentialCategories.map((row) => (
+                                        <TableRow align="left" key={row}>
+                                            <TableCell sx={{width:'10%', paddingLeft: '50px'}} component="th" scope="row" >
+                                                {row}
+                                            </TableCell>
+                                            {Array(testData.actions.length).fill().map((_, index) => (
+                                                <TableCell align="center" key={index}>
+                                                    <Checkbox />
+                                                </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+}
 
 const NotificationPage: NextPage = () => {
     usePublishTab({ title: "Notifications", path: "/notification" });
 
     const { data: notifications } = useGetNotificationsQuery();
 
-    // // TODO: test data; remove
-    // const { data: notification1 } = useGetNotificationByIdQuery(1);
-    // const { data: notification2 } = useGetNotificationByIdQuery(2);
-
-    // const notifications = useMemo(() => {
-    //     if (!notification1 && !notification2) return [];
-    //     if (notification1 && !notification2) return [notification1];
-    //     if (!notification1 && notification2) return [notification2];
-    //     return [notification1, notification2];
-    // }, [notification1, notification2]);
-
-    const rows = useMemo(
-        () =>
-            notifications?.map((notification: ContactNotification) =>
-                createRow(notification)
-            ) || [],
-        [notifications]
-    );
-
-    const handleRemove = (index: number) => {
-        console.log("will delete notification: ", index);
-    };
+    // const { data: data } = useAllGlobalsQuery();
+    const testData = createTestData();
 
     return (
-        <>
-            <Paper sx={{ flex: 1, mt: 1 }}>
-                <CollapsibleTable rows={rows} onRemove={handleRemove} />
-            </Paper>
-        </>
+        <TableContainer component={Paper} style={ {width: '1500px'}}>
+            <Table aria-label="collapsible table">
+                <TableHead>
+                    <TableRow sx={{ background:'grey' }}>
+                        <TableCell sx={{width:'10%'}} align="left"></TableCell>
+                        {testData.actions.map((row) => (
+                            <TableCell sx={{width:'10%'}} align="left" key={row}> {row} </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {testData.parentCategories.map((row) => (
+                        <Row key={row} row={row} />
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
-};
+}
 
 NotificationPage.getLayout = (page) => (
     <AuthGuard>
@@ -55,3 +165,4 @@ NotificationPage.getLayout = (page) => (
 );
 
 export default NotificationPage;
+
