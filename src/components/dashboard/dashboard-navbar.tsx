7bbbb9d97f660be0d202e10d971738119fb2be10
npyl <n.pylarinos@hotmail.com>
2023-10-04 12:31:19 +0300

    version

--- src/components/dashboard/dashboard-navbar.tsx
+++ src/components/dashboard/dashboard-navbar.tsx
@@ -125,7 +125,7 @@ export const DashboardNavbar: FC<DashboardNavbarProps> = (props) => {
                                 }}
                             />
                             <Typography color="neutral.900" variant={"h5"}>
-                                Mordor v0.32
+                                Mordor v0.33
                             </Typography>
                         </Link>
                     </Box>
