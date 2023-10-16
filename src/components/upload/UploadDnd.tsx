c0571f71d34e925c5b3ad3fecc710120abfd2e5a
npyl <n.pylarinos@hotmail.com>
2023-10-16 14:38:53 +0300

    ui

--- src/components/upload/UploadDnd.tsx
+++ src/components/upload/UploadDnd.tsx
@@ -143,6 +143,7 @@ export default function UploadDnd({
                 <>
                     <Box
                         sx={{
+                            mt: 1,
                             overflowY: "auto",
                         }}
                     >
