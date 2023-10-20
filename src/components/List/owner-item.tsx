53a7db23d950c036111377dd6889bfa2693db8ce
taxiarchiszaronis <taxiarchiszaronis@digipath.gr>
2023-10-20 14:15:18 +0300

    owner manager at basic details

--- src/components/List/owner-item.tsx
+++ src/components/List/owner-item.tsx
@@ -1,11 +1,11 @@
-import { Avatar, Box, Button, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography, Fade } from "@mui/material";
 import { useRouter } from "next/router";
 import type { FC } from "react";
-import { IUser } from "src/types/user";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import ListItem from "./item";
 import { useProfileQuery } from "src/services/user";
 import { ICustomer } from "src/types/customer";
-import { useTranslation } from "react-i18next";
 
 interface ListOwnerItemProps {
     owner: ICustomer;
@@ -17,61 +17,81 @@ const ListOwnerItem: FC<ListOwnerItemProps> = (props) => {
     const { owner, label = t("Owner"), ...other } = props;
     const router = useRouter();
     const { data } = useProfileQuery();
-    if (!data) return null;
+    const [showOwner, setShowOwner] = useState(false);
+
     const performViewOwner = () => {
-        // view Owner
-        router.push(`/customer/${owner.id}`);
+        router.push(`/customer/${owner?.id}`);
     };
 
+    const handleTouchStart = () => {
+        setShowOwner(true);
+    };
+
+    const handleTouchEnd = () => {
+        setShowOwner(false);
+    };
+    const fitContentStyles = {
+        display: "inline-flex", // makes the container fit its content width
+        alignItems: "center",
+        justifyContent: "center", // horizontally aligns the button's content
+        height: "auto", // adapts the height based on the content
+        p: 0, // removes padding
+        borderRadius: "12px",
+        borderColor: "transparent",
+        "&:hover": {
+            backgroundColor: "rgba(0, 0, 0, 0.04)",
+        },
+        "&:active": {
+            borderColor: "primary.main",
+        },
+    };
+    if (!data) return null;
     return (
         <ListItem label={label} {...other}>
             <Button
-                sx={{
-                    flex: 1,
-                    float: "right",
-                    height: "20px",
-                    borderRadius: 0,
-                    padding: 1,
-                    width: "100px",
-                    borderColor: "#D3D3D3",
-                    "&:active": {
-                        borderColor: "black",
-                    },
-                }}
+                sx={fitContentStyles} // apply styles
                 variant="outlined"
                 onClick={performViewOwner}
+                onMouseOver={handleTouchStart}
+                onMouseLeave={handleTouchEnd}
             >
                 <Box
-                    display="flex"
-                    alignItems="center"
-                    bgcolor="white"
-                    borderRadius={0}
-                    width="100%"
                     sx={{
-                        height: "15px",
+                        ...fitContentStyles, // apply the same styles here
+                        justifyContent: "space-between",
+                        bgcolor: "grey.300",
+                        borderRadius: "inherit",
+                        boxShadow: 1,
+                        px: 1, // horizontal padding
+                        boxSizing: "border-box",
                     }}
                 >
                     <Avatar
                         sx={{
-                            bgcolor: "white",
+                            bgcolor: "primary.light",
                             color: "grey.900",
-                            width: 18,
-                            height: 18,
+                            width: 20,
+                            height: 20,
+                            mr: 1, // adds some margin to the right of the avatar, you can adjust as needed
                         }}
                     ></Avatar>
 
-                    <Typography
-                        variant="subtitle2"
-                        sx={{
-                            overflow: "hidden",
-                            color: "#1f2124",
-                            fontSize: "0.8rem",
-                            marginLeft: "auto", // Centers the Typography inside the flex container
-                            marginRight: "auto", // Centers the Typography inside the flex container
-                        }}
-                    >
-                        {owner?.lastName}
-                    </Typography>
+                    {/* Use Fade for smooth transition */}
+                    <Fade in={showOwner}>
+                        <Typography
+                            variant="subtitle2"
+                            noWrap
+                            component="div"
+                            sx={{
+                                overflow: "hidden",
+                                textOverflow: "ellipsis",
+                                whiteSpace: "nowrap",
+                                // remove maxWidth restriction here to allow the text to expand naturally
+                            }}
+                        >
+                            {owner?.lastName}
+                        </Typography>
+                    </Fade>
                 </Box>
             </Button>
         </ListItem>
