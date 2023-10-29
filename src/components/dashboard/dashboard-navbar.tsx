80834d53a80e512bd09b7f681ec2001bd3483c84
npyl <n.pylarinos@hotmail.com>
2023-10-29 12:45:02 +0200

    version v0.50

--- src/components/dashboard/dashboard-navbar.tsx
+++ src/components/dashboard/dashboard-navbar.tsx
@@ -125,7 +125,7 @@ export const DashboardNavbar: FC<DashboardNavbarProps> = (props) => {
                                 }}
                             />
                             <Typography color="neutral.900" variant={"h5"}>
-                                PropertyPro v0.49
+                                PropertyPro v0.50
                             </Typography>
                         </Link>
                     </Box>
