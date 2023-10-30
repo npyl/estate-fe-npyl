04d2774fe68616ff40022ad44a612f9f3be5ebe8
npyl <npylarinos@digipath.gr>
2023-10-30 17:08:41 +0200

    .

--- src/components/List/StyledListItem.tsx
+++ src/components/List/StyledListItem.tsx
@@ -2,13 +2,19 @@ import { styled } from "@mui/material/styles";
 import { ListItem as MuiListItem } from "@mui/material";
 
 const StyledListItem = styled(MuiListItem)(({ theme }) => ({
- flex: 1,
-  "&:nth-of-type(odd)": {
-    background: theme.palette.mode === "dark" ? "transparent" : theme.palette.background.paper,
-  },
-  "&:nth-of-type(even)": {
-    background: theme.palette.mode === "dark" ? "transparent" : "#fcfcfc",
-  },
+    flex: 1,
+    "&:nth-of-type(odd)": {
+        background:
+            theme.palette.mode === "dark"
+                ? "transparent"
+                : theme.palette.background.paper,
+    },
+    "&:nth-of-type(even)": {
+        background:
+            theme.palette.mode === "dark"
+                ? theme.palette.background.default
+                : theme.palette.grey[50],
+    },
 }));
 
 export default StyledListItem;
