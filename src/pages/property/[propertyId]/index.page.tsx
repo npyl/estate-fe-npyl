02420bcc45d6c7525e027147e1d9cd3a86bb6d79
npyl <pylarinosnick@gmail.com>
2023-12-07 09:40:42 +0200

    Property View Suspense

--- src/pages/property/[propertyId]/index.page.tsx
+++ src/pages/property/[propertyId]/index.page.tsx
@@ -1,11 +1,10 @@
 import { Box, Tab, Tabs } from "@mui/material";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { Suspense, useState } from "react";
 import {
     useClonePropertyMutation,
     useDeletePropertyMutation,
-    useGetPropertyByIdQuery,
 } from "src/services/properties";
 
 import TabPanel from "src/components/Tabs";
@@ -13,32 +12,12 @@ import { AuthGuard } from "src/components/authentication/auth-guard";
 import { DashboardLayout } from "src/components/dashboard/dashboard-layout";
 import MainContainer from "./MainContainer";
 
-import {
-    AddressSection,
-    AreaSection,
-    BalconiesSection,
-    BasicSection,
-    BlueprintsSection,
-    DetailsSection,
-    DistanceSection,
-    HeatingSection,
-    ImageSection,
-    NotesSection,
-    ParkingsSection,
-    SuitableFor,
-    TechnicalFeatures,
-    VideoSection,
-} from "./sections";
-
 import ViewHeader from "src/pages/components/ViewHeader";
 
 import "photoswipe/dist/photoswipe.css";
 import InitMap from "./Map";
-import ConstructionSection from "./sections/ConstructionSection";
-import DescriptionSection from "./sections/DescriptionSection";
-import Features from "./sections/FeaturesSection";
 import MatchingCustomersSection from "./sections/MatchingCustomers";
-import PhotosOnly from "./sections/PhotosOnly";
+import { PhotosOnly } from "./sections/PhotosOnly";
 
 import { useTranslation } from "react-i18next";
 
@@ -58,26 +37,15 @@ function a11yProps(index: number) {
 const SingleProperty: NextPage = () => {
     const router = useRouter();
     const { t } = useTranslation();
-    const { removeTab, pushTab } = useTabsContext();
+    const { removeTab } = useTabsContext();
 
     const { propertyId } = router.query;
 
-    const { data } = useGetPropertyByIdQuery(+propertyId!); // basic details
     const [cloneProperty] = useClonePropertyMutation();
     const [deleteProperty] = useDeletePropertyMutation();
 
     const [value, setValue] = useState(0);
     const [cloneConfirmDialogOpen, setCloneConfirmDialogOpen] = useState(false);
-
-    useEffect(() => {
-        if (data && propertyId) {
-            pushTab({
-                path: `/property/${propertyId}`,
-                id: propertyId as string,
-                label: `Property ${data?.code || ""}`,
-            });
-        }
-    }, [data, propertyId]);
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) =>
         setValue(newValue);
@@ -101,8 +69,6 @@ const SingleProperty: NextPage = () => {
             router.push("/");
             removeTab(propertyId as string);
         });
-
-    if (!data) return null;
 
     return (
         <>
@@ -129,27 +95,9 @@ const SingleProperty: NextPage = () => {
             </ViewHeader>
             <Box height={"100%"}>
                 <TabPanel value={value} index={0}>
-                    <MainContainer
-                        AddressSection={<AddressSection />}
-                        ImageSection={<ImageSection data={data} />}
-                        BasicSection={<BasicSection data={data} />}
-                        DetailsSection={<DetailsSection data={data} />}
-                        HeatingSection={<HeatingSection data={data} />}
-                        AreaSection={<AreaSection data={data} />}
-                        DistanceSection={<DistanceSection data={data} />}
-                        ParkingsSection={<ParkingsSection data={data} />}
-                        BalconiesSection={<BalconiesSection data={data} />}
-                        BlueprintsSection={<BlueprintsSection data={data} />}
-                        NotesSection={<NotesSection />}
-                        VideoSection={<VideoSection data={data} />}
-                        SuitableFor={<SuitableFor data={data} />}
-                        TechnicalFeatures={<TechnicalFeatures data={data} />}
-                        DescriptionSection={<DescriptionSection data={data} />}
-                        ConstructionSection={
-                            <ConstructionSection data={data} />
-                        }
-                        Features={<Features data={data as any} />}
-                    />
+                    <Suspense fallback={<span>Loading...</span>}>
+                        <MainContainer />
+                    </Suspense>
                 </TabPanel>
                 <TabPanel value={value} index={1}></TabPanel>
                 <TabPanel value={value} index={2}></TabPanel>
@@ -157,7 +105,9 @@ const SingleProperty: NextPage = () => {
                     <MatchingCustomersSection />
                 </TabPanel>
                 <TabPanel value={value} index={4}>
-                    <PhotosOnly data={data as any} />
+                    <Suspense fallback={<span>Loading...</span>}>
+                        <PhotosOnly />
+                    </Suspense>
                 </TabPanel>
                 <TabPanel value={value} index={5}>
                     <Integrations />
