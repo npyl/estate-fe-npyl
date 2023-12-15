108dbbf279596006e79576d738a7c55743e7e8ab
npyl <pylarinosnick@gmail.com>
2023-12-15 14:40:13 +0200

    v0.61

--- src/components/dashboard/dashboard-navbar.tsx
+++ src/components/dashboard/dashboard-navbar.tsx
@@ -141,7 +141,7 @@ export const DashboardNavbar: FC<DashboardNavbarProps> = (props) => {
                             {/* nick ama to svhseis se gamhsa */}
                             <Tooltip title="Alex Gamiesai file">
                                 <StyledTypography variant="h5">
-                                    PropertyPro v0.60
+                                    PropertyPro v0.61
                                 </StyledTypography>
                             </Tooltip>
                         </Link>
