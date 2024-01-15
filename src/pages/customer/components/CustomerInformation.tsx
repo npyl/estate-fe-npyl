70742087f83969abe1a0a36831af94cff2abb89b
e-radio-fk <79450488+e-radio-fk@users.noreply.github.com>
2024-01-15 11:43:11 +0200

    Customer: RHF: cleanup customer information page using the FIELDS technique

--- src/pages/customer/components/CustomerInformation.tsx
+++ src/pages/customer/components/CustomerInformation.tsx
@@ -16,6 +16,60 @@ import { useTranslation } from "react-i18next";
 import { useRouter } from "next/router";
 import Panel from "src/components/Panel";
 import { RHFSelect, RHFTextField } from "src/components/hook-form";
+import { TranslationType } from "src/types/translation";
+import { useMemo } from "react";
+import { IUser } from "src/types/user";
+import { KeyValue } from "src/types/KeyValue";
+
+const getFIELDS = (
+    t: TranslationType,
+    managers?: IUser[],
+    nationalitiesEnum?: KeyValue<string>[],
+    leadSourceEnum?: KeyValue<string>[]
+) => [
+    <RHFTextField fullWidth name="firstName" label={t("First Name")} />,
+    <RHFTextField fullWidth name="lastName" label={t("Last Name")} />,
+    <RHFTextField fullWidth name="email" label={t("Email")} />,
+    <RHFSelect name="managedBy" label={t("Managed By")}>
+        {managers?.map(({ id, firstName, lastName }, i) => (
+            <MenuItem key={i} value={id}>
+                {`${firstName} ${lastName}`}
+            </MenuItem>
+        ))}
+    </RHFSelect>,
+    <RHFTextField fullWidth name="mobilePhone" label={t("Mobile Phone")} />,
+    <RHFTextField fullWidth name="homePhone" label={t("Home Phone")} />,
+    <RHFTextField fullWidth name="fax" label={t("Fax")} />,
+    <RHFSelect fullWidth name="nationality" label={t("Nationality")}>
+        {nationalitiesEnum?.map(({ key, value }, i) => (
+            <MenuItem key={i} value={key}>
+                {value}
+            </MenuItem>
+        ))}
+    </RHFSelect>,
+    <RHFTextField fullWidth name="idNumber" label={t("ID Number")} />,
+    <RHFTextField fullWidth name="dateOfBirth" label={t("Date of Birth")} />,
+    <RHFTextField
+        fullWidth
+        name="passportNumber"
+        label={t("Passport Number")}
+    />,
+    <RHFSelect
+        fullWidth
+        name="preferredLanguage"
+        label={t("Preferred Language")}
+    >
+        <MenuItem value={"ENGLISH"}>{t("English")}</MenuItem>
+        <MenuItem value={"GREEK"}>{t("Greek")}</MenuItem>
+    </RHFSelect>,
+    <RHFSelect fullWidth name="leadSource" label={t("Lead Source")}>
+        {leadSourceEnum?.map(({ key, value }, i) => (
+            <MenuItem key={i} value={key}>
+                {value}
+            </MenuItem>
+        ))}
+    </RHFSelect>,
+];
 
 const CustomerInformation: React.FC<any> = () => {
     const dispatch = useDispatch();
@@ -31,179 +85,78 @@ const CustomerInformation: React.FC<any> = () => {
     const managers = useAllUsersQuery().data;
 
     const status = useSelector(selectStatus) || 0;
-
     const leadSource = (useSelector(selectLeadSource) as LeadSource) || "";
+
+    const FIELDS = useMemo(
+        () => getFIELDS(t, managers, nationalitiesEnum, leadSourceEnum),
+        [t, managers, nationalitiesEnum, leadSourceEnum]
+    );
 
     return (
         <Panel label="Customer Information">
-            <Grid item xs={12} padding={1}>
-                <Grid container spacing={2}>
-                    <Grid item xs={6}>
+            <Grid container spacing={2} p={1.5}>
+                {FIELDS.map((f, i) => (
+                    <Grid key={i} item xs={6}>
+                        {f}
+                    </Grid>
+                ))}
+
+                <Grid item xs={6}>
+                    {leadSource === "CUSTOMER" ? (
                         <RHFTextField
                             fullWidth
-                            name="firstName"
-                            label={t("First Name")}
+                            name="suggestedBy"
+                            label={t("Suggested by")}
                         />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFTextField
-                            fullWidth
-                            name="lastName"
-                            label={t("Last Name")}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFTextField
-                            fullWidth
-                            name="email"
-                            label={t("Email")}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFSelect name="managedBy" label={t("Managed By")}>
-                            {managers?.map(({ id, firstName, lastName }, i) => (
-                                <MenuItem key={i} value={id}>
-                                    {`${firstName} ${lastName}`}
-                                </MenuItem>
-                            ))}
-                        </RHFSelect>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFTextField
-                            fullWidth
-                            name="mobilePhone"
-                            label={t("Mobile Phone")}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFTextField
-                            fullWidth
-                            name="homePhone"
-                            label={t("Home Phone")}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFTextField fullWidth name="fax" label={t("Fax")} />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFSelect
-                            fullWidth
-                            name="nationality"
-                            label={t("Nationality")}
-                        >
-                            {nationalitiesEnum?.map(({ key, value }, i) => (
-                                <MenuItem key={i} value={key}>
-                                    {value}
-                                </MenuItem>
-                            ))}
-                        </RHFSelect>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFTextField
-                            fullWidth
-                            name="idNumber"
-                            label={t("ID Number")}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFTextField
-                            fullWidth
-                            name="dateOfBirth"
-                            label={t("Date of Birth")}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFTextField
-                            fullWidth
-                            name="passportNumber"
-                            label={t("Passport Number")}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFSelect
-                            fullWidth
-                            name="preferredLanguage"
-                            label={t("Preferred Language")}
-                        >
-                            <MenuItem value={"ENGLISH"}>
-                                {t("English")}
-                            </MenuItem>
-                            <MenuItem value={"GREEK"}>{t("Greek")}</MenuItem>
-                        </RHFSelect>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <RHFSelect
-                            fullWidth
-                            name="leadSource"
-                            label={t("Lead Source")}
-                        >
-                            {leadSourceEnum?.map(({ key, value }, i) => (
-                                <MenuItem key={i} value={key}>
-                                    {value}
-                                </MenuItem>
-                            ))}
-                        </RHFSelect>
-                    </Grid>
-                    <Grid item xs={6}>
-                        {leadSource === "CUSTOMER" ? (
-                            <RHFTextField
-                                fullWidth
-                                name="suggestedBy"
-                                label={t("Suggested by")}
-                            />
-                        ) : null}
-                    </Grid>
-                    <Grid item xs={6}>
+                    ) : null}
+                </Grid>
+                <Grid item xs={6}>
+                    <Box
+                        sx={{
+                            border: 1,
+                            borderColor: "divider",
+                            borderRadius: 1,
+                            height: "100%",
+                            px: 3,
+                            py: 1.5,
+                            display: "flex",
+                            justifyContent: "center",
+                        }}
+                        flexDirection={"column"}
+                    >
                         <Box
                             sx={{
-                                border: 1,
-                                borderColor: "divider",
-                                borderRadius: 1,
-                                height: "100%",
-                                px: 3,
-                                py: 1.5,
                                 display: "flex",
                                 justifyContent: "center",
                             }}
-                            flexDirection={"column"}
                         >
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    justifyContent: "center",
-                                }}
-                            >
-                                <Typography variant="h6">
-                                    {t("Status")}
-                                </Typography>
-                            </Box>
-
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    py: 1.5,
-                                }}
-                            >
-                                <Rating
-                                    name="simple-controlled"
-                                    value={status}
-                                    onChange={(_event, newValue) => {
-                                        dispatch(setStatus(newValue));
-                                    }}
-                                />
-                            </Box>
+                            <Typography variant="h6">{t("Status")}</Typography>
                         </Box>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <LabelCreate
-                            variant="customer"
-                            resourceId={customerId ? +customerId : -1}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <CustomerTypeSelect />
-                    </Grid>
+
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                py: 1.5,
+                            }}
+                        >
+                            <Rating
+                                value={status}
+                                onChange={(_event, newValue) => {
+                                    dispatch(setStatus(newValue));
+                                }}
+                            />
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={6}>
+                    <LabelCreate
+                        variant="customer"
+                        resourceId={customerId ? +customerId : -1}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <CustomerTypeSelect />
                 </Grid>
             </Grid>
         </Panel>
