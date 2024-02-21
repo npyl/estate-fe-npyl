90cf388ce041bfbbc95756b24c0d8a57d7873f46
e-radio-fk <79450488+e-radio-fk@users.noreply.github.com>
2024-02-21 13:41:40 +0200

    v.

--- src/components/dashboard/dashboard-navbar.tsx
+++ src/components/dashboard/dashboard-navbar.tsx
@@ -130,7 +130,7 @@ export const DashboardNavbar: FC<DashboardNavbarProps> = (props) => {
                             {/* nick ama to svhseis se gamhsa */}
                             <Tooltip title="Alex Gamiesai file">
                                 <StyledTypography variant="h5">
-                                    PropertyPro v0.72
+                                    PropertyPro v0.73
                                 </StyledTypography>
                             </Tooltip>
                         </Link>
