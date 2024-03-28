95a88e99df8314323ac63a10ce2673a34440cbcd
npyl <pylarinosnick@gmail.com>
2024-03-28 14:56:25 +0200

    Improve.

--- src/pages/property/edit/[propertyId]/forms/_general/Rent.tsx
+++ src/pages/property/edit/[propertyId]/forms/_general/Rent.tsx
@@ -1,13 +1,22 @@
-import { Grid } from "@mui/material";
-import { useCallback, useEffect, useState } from "react";
+import { Grid, Box } from "@mui/material";
+import { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { DateObject } from "react-multi-date-picker";
 import {
     RHFCheckbox,
     RHFDatePicker,
     RHFOnlyNumbers,
 } from "src/components/hook-form";
+import { styled } from "@mui/material/styles";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+    border: "1px dashed",
+    borderColor:
+        theme.palette.mode === "light"
+            ? theme.palette.neutral?.[300]
+            : theme.palette.neutral?.[600],
+    borderRadius: "10px",
+}));
 
 const Rent = () => {
     const { t } = useTranslation();
@@ -27,56 +36,50 @@ const Rent = () => {
     }, [startDate]);
 
     return (
-        <Grid item xs={12} p={1}>
+        <>
             <RHFCheckbox name="rented" label={t("Rented")} />
 
-            <Grid
-                container
-                spacing={0}
-                sx={{
-                    padding: "10px",
-                    border: "1px solid #000000",
-                    borderRadius: "10px",
-                }}
-            >
-                <Grid item xs={12}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <RHFDatePicker
-                                name="availableAfter"
-                                label={t("Available After").toString()}
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <RHFOnlyNumbers
-                                name="currentRentPrice"
-                                label={t("Current Rent Price")}
-                                adornment="€"
-                                disabled={!rented}
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <RHFDatePicker
-                                name="rentalStart"
-                                pickerProps={{
-                                    minDate: availDate,
-                                }}
-                                label={t("Rental Period Start").toString()}
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <RHFDatePicker
-                                name="rentalEnd"
-                                label={t("Rental Period End").toString()}
-                                pickerProps={{
-                                    minDate: startDate,
-                                }}
-                            />
-                        </Grid>
+            <StyledBox p={1}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} lg={6}>
+                        <RHFDatePicker
+                            fullWidth
+                            name="availableAfter"
+                            label={t("Available After").toString()}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} lg={6}>
+                        <RHFOnlyNumbers
+                            name="currentRentPrice"
+                            label={t("Current Rent Price")}
+                            adornment="€"
+                            disabled={!rented}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} lg={6}>
+                        <RHFDatePicker
+                            name="rentalStart"
+                            pickerProps={{
+                                minDate: availDate,
+                            }}
+                            label={t("Rental Period Start").toString()}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} lg={6}>
+                        <RHFDatePicker
+                            name="rentalEnd"
+                            label={t("Rental Period End").toString()}
+                            pickerProps={{
+                                minDate: startDate,
+                            }}
+                        />
                     </Grid>
                 </Grid>
-            </Grid>
-        </Grid>
+            </StyledBox>
+        </>
     );
 };
 
