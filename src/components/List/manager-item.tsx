1d4c0c7f179f74bc888fb37b27d84cacbbda3769
npyl <e-radio-fk@gmail.com>
2024-03-30 10:31:01 +0200

    Improve manager item for dark mode.

--- src/components/List/manager-item.tsx
+++ src/components/List/manager-item.tsx
@@ -1,90 +1,94 @@
-import { Avatar, Box, Button, Fade, Typography } from "@mui/material";
+import { Avatar, Button, Fade, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { useRouter } from "next/router";
-import { FC, useState } from "react";
+import { FC, useCallback, useState } from "react";
 import { IUser } from "src/types/user";
 import ListItem from "./item";
 import { useProfileQuery } from "src/services/user";
-import { ICustomer } from "src/types/customer";
 import { useTranslation } from "react-i18next";
+
+// -----------------------------------------------------------
+
+const CustomButton = styled(Button)(({ theme }) => ({
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "auto",
+
+    // INFO: appears better to my eyes.
+    marginRight: theme.spacing(-1),
+
+    paddingTop: theme.spacing(0),
+    paddingBottom: theme.spacing(0),
+    paddingLeft: theme.spacing(0.1),
+    paddingRight: theme.spacing(0.5),
+
+    borderRadius: "12px",
+    borderColor: "transparent",
+
+    color: theme.palette.text.primary,
+
+    backgroundColor:
+        theme.palette.mode === "dark"
+            ? theme.palette.neutral?.[700]
+            : theme.palette.neutral?.[100],
+
+    gap: theme.spacing(1),
+
+    "&:active": {
+        borderColor: theme.palette.primary.main,
+    },
+}));
+
+const CustomAvatar = styled(Avatar)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.grey?.[900],
+    width: 20,
+    height: 20,
+}));
+
+// -----------------------------------------------------------
 
 interface ListManagerItemProps {
     manager: IUser;
     label?: string | any;
 }
 
-const ListManagerItem: FC<ListManagerItemProps> = (props) => {
+const ListManagerItem: FC<ListManagerItemProps> = ({ manager, label }) => {
     const { t } = useTranslation();
-    const { manager, label = t("Manager"), ...other } = props;
     const router = useRouter();
+
     const [showManager, setShowManager] = useState(true);
     const { data } = useProfileQuery();
 
-    const performViewManager = () => {
-        router.push(`/user/${manager.id}`);
-    };
+    const handleClick = useCallback(
+        () => router.push(`/user/${manager.id}`),
+        []
+    );
 
-    const fitContentStyles = {
-        display: "inline-flex", // makes the container fit its content width
-        alignItems: "center",
-        justifyContent: "center", // horizontally aligns the button's content
-        height: "auto", // adapts the height based on the content
-        p: 0, // removes padding
-        borderRadius: "12px",
-        borderColor: "transparent",
-        "&:hover": {
-            backgroundColor: "rgba(0, 0, 0, 0.04)",
-        },
-        "&:active": {
-            borderColor: "primary.main",
-        },
-    };
     if (!data) return null;
-    return (
-        <ListItem label={label} {...other}>
-            <Button
-                sx={fitContentStyles} // apply styles
-                variant="outlined"
-                onClick={performViewManager}
-            >
-                <Box
-                    sx={{
-                        ...fitContentStyles, // apply the same styles here
-                        justifyContent: "space-between",
-                        bgcolor: "grey.300",
-                        borderRadius: "inherit",
-                        boxShadow: 1,
-                        px: 1, // horizontal padding
-                        boxSizing: "border-box",
-                    }}
-                >
-                    <Avatar
-                        sx={{
-                            bgcolor: "primary.light",
-                            color: "grey.900",
-                            width: 20,
-                            height: 20,
-                            mr: 1, // adds some margin to the right of the avatar, you can adjust as needed
-                        }}
-                    ></Avatar>
 
-                    {/* Use Fade for smooth transition */}
-                    <Fade in={showManager}>
-                        <Typography
-                            variant="subtitle2"
-                            noWrap
-                            component="div"
-                            sx={{
-                                overflow: "hidden",
-                                textOverflow: "ellipsis",
-                                whiteSpace: "nowrap",
-                                // remove maxWidth restriction here to allow the text to expand naturally
-                            }}
-                        >
-                            {manager?.lastName}
-                        </Typography>
-                    </Fade>
-                </Box>
-            </Button>
+    return (
+        <ListItem label={label || t("Manager")}>
+            <CustomButton variant="outlined" onClick={handleClick}>
+                <CustomAvatar />
+
+                {/* Use Fade for smooth transition */}
+                <Fade in={showManager}>
+                    <Typography
+                        noWrap
+                        variant="subtitle2"
+                        component="div"
+                        sx={{
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            whiteSpace: "nowrap",
+                        }}
+                    >
+                        {manager?.lastName}
+                    </Typography>
+                </Fade>
+            </CustomButton>
         </ListItem>
     );
 };
