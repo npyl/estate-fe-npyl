8b14efa43e28f73fda9cb8967c66a4ade14ca6f8
npyl <pylarinosnick@gmail.com>
2024-04-01 10:18:43 +0300

    Labels list item. 1/2

--- src/components/List/Items/labels.tsx
+++ src/components/List/Items/labels.tsx
@@ -1,37 +1,33 @@
-import { ListItemProps, Typography, Box } from "@mui/material";
+import { Stack } from "@mui/material";
 import type { FC } from "react";
-import Label from "../../label/Label";
-
+import Label from "@/components/label/Label";
 import { ILabel } from "src/types/label";
 import ListItem from "../item";
-
-type Direction = "horizontal" | "vertical";
+import { useTranslation } from "react-i18next";
+import ListItemProps from "../types";
 
 interface ListLabelsItemProps extends ListItemProps {
     labels: ILabel[];
-    align?: Direction;
-    label: string;
 }
 
-// This function truncates the label name if it's more than 16 characters
-const truncateLabelName = (name: string) => {
-    return name.length > 19 ? name.substring(0, 19) + "..." : name;
-};
-
-const ListLabelsItem: FC<ListLabelsItemProps> = (props) => {
-    const { labels, label = "Labels", ...other } = props;
+const ListLabelsItem: FC<ListLabelsItemProps> = ({ labels, ...other }) => {
+    const { t } = useTranslation();
 
     return (
-        <ListItem label={label} {...other}>
-            <Box paddingLeft={0} margin={0} display="flex" flexWrap="wrap">
-                {labels.map((label, index) => {
-                    return (
-                        <Label key={index} sx={{ bgcolor: label.color, mr: 1 }}>
-                            {truncateLabelName(label.name)}
-                        </Label>
-                    );
-                })}
-            </Box>
+        <ListItem
+            {...other}
+            label={t("Labels").toString()}
+            sx={{
+                height: "fit-content",
+            }}
+        >
+            <Stack spacing={1}>
+                {labels.map(({ color, name }, i) => (
+                    <Label key={i} sx={{ bgcolor: color, mr: 1 }}>
+                        {name}
+                    </Label>
+                ))}
+            </Stack>
         </ListItem>
     );
 };
