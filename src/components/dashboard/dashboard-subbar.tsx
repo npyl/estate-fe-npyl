ee3384fa40e6950a58adbf1eef65dfa11a7d8b42
npyl <pylarinosnick@gmail.com>
2024-04-02 10:26:51 +0300

    Subbar items dark mode.

--- src/components/dashboard/dashboard-subbar.tsx
+++ src/components/dashboard/dashboard-subbar.tsx
@@ -1,10 +1,56 @@
 import ClearIcon from "@mui/icons-material/Clear";
-import { Button, Divider, IconButton } from "@mui/material";
+import { Button, ButtonProps, IconButton, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
-import { Fragment, useMemo } from "react";
+import { useMemo } from "react";
 import { ScrollBox } from "../ScrollBox";
 import { useTabsContext } from "src/contexts/tabs";
 import useAutosaveRouter from "src/components/Router/Autosave";
+import { styled } from "@mui/material/styles";
+import { getBorderColor2 } from "@/theme/borderColor";
+
+interface SubbarItemProps extends ButtonProps {
+    current: boolean;
+}
+
+const SubbarItem = styled(Button)<SubbarItemProps>(({ theme, current }) => ({
+    border: "1px solid",
+    borderColor: getBorderColor2(theme),
+
+    ...(current
+        ? {
+              color: theme.palette.neutral?.[200],
+              backgroundColor: theme.palette.primary.main,
+
+              "&:hover": {
+                  color: theme.palette.neutral?.[100],
+                  backgroundColor: theme.palette.primary.dark,
+              },
+          }
+        : {
+              color:
+                  theme.palette.mode === "light"
+                      ? theme.palette.neutral?.[900]
+                      : theme.palette.neutral?.[200],
+
+              backgroundColor:
+                  theme.palette.mode === "light"
+                      ? theme.palette.background.paper
+                      : theme.palette.neutral?.[800],
+          }),
+
+    boxShadow: theme.shadows[5],
+
+    paddingTop: theme.spacing(0.5),
+    paddingBottom: theme.spacing(0.5),
+    paddingLeft: theme.spacing(1.5),
+
+    transition: "all 0.3s ease",
+
+    cursor: "pointer",
+
+    flexDirection: "row",
+    alignItems: "center",
+}));
 
 const Subbar = () => {
     const router = useAutosaveRouter();
@@ -15,81 +61,22 @@ const Subbar = () => {
     return (
         <ScrollBox sx={{ overflowX: "auto" }}>
             <Stack direction={"row"} spacing={1}>
-                {appTabs.map((tab, index) => (
-                    <Fragment key={tab.id}>
-                        <Stack
-                            sx={{
-                                bgcolor:
-                                    currentPath === tab.path
-                                        ? "primary.dark"
-                                        : "darkenColor",
-                                padding: "0px 0px",
-                                borderRadius: 1,
-                                boxShadow: `rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px`,
-                                transition: "all 0.3s ease",
-                                "&:hover": {
-                                    transform: "scale(1.05)",
-                                    bgcolor:
-                                        currentPath === tab.path
-                                            ? "primary.dark"
-                                            : "darkenColor", // replace "darkenColor" with the darker color for the unset state
-                                },
-                            }}
-                            direction={"row"}
-                            alignItems={"center"}
-                        >
-                            <Button
-                                sx={{
-                                    minWidth: "140px",
-                                    maxWidth: "140px",
-                                    height: "30px",
-                                    color:
-                                        currentPath === tab.path
-                                            ? "white"
-                                            : "unset",
-                                    "&:hover": {
-                                        background: "transparent",
-                                    },
-                                    fontSize: "0.8rem",
-                                    display: "flex",
-                                    direction: "rtl",
-                                    whiteSpace: "nowrap",
-                                    overflow: "hidden",
-                                }}
-                                variant="text"
-                                onClick={() => router.push(tab.path)}
-                            >
-                                <span
-                                    style={{
-                                        direction: "ltr",
-                                        whiteSpace: "nowrap",
-                                        overflow: "hidden",
-                                        textOverflow: "ellipsis",
+                {appTabs.map(({ id, label, path }) => (
+                    <SubbarItem
+                        key={id}
+                        current={currentPath === path}
+                        endIcon={
+                            <IconButton onClick={() => removeTab(id)}>
+                                <ClearIcon
+                                    sx={{
+                                        fontSize: "15px",
                                     }}
-                                >
-                                    {tab.label}
-                                </span>
-                            </Button>
-
-                            <IconButton
-                                sx={{
-                                    color:
-                                        currentPath === tab.path
-                                            ? "white"
-                                            : "unset",
-                                    "&:hover": {
-                                        background: "transparent",
-                                    },
-                                }}
-                                onClick={() => removeTab(tab.id)}
-                            >
-                                <ClearIcon sx={{ fontSize: 12 }} />
+                                />
                             </IconButton>
-                        </Stack>
-                        {index !== appTabs.length - 1 && (
-                            <Divider orientation="vertical" flexItem />
-                        )}
-                    </Fragment>
+                        }
+                    >
+                        <Typography variant="body2">{label}</Typography>
+                    </SubbarItem>
                 ))}
             </Stack>
         </ScrollBox>
