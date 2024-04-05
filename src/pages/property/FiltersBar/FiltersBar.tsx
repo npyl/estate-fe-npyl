cc38fc7ad4565e53fc1844e9fc45b063acbf4e9b
npyl <pylarinosnick@gmail.com>
2024-04-05 13:04:11 +0300

    Alternative responsiveness for FilterBar / FilterMore.

--- src/pages/property/FiltersBar/FiltersBar.tsx
+++ src/pages/property/FiltersBar/FiltersBar.tsx
@@ -3,10 +3,10 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import { Box, SvgIconTypeMap } from "@mui/material";
+import { Badge, Box, SvgIconTypeMap } from "@mui/material";
 // redux
-import { useSelector } from "react-redux";
-import { sumOfChangedProperties } from "src/slices/filters";
+import { useDispatch, useSelector } from "react-redux";
+import { sumOfChangedProperties, resetState } from "src/slices/filters";
 // icons
 import { Menu } from "src/icons/menu";
 import GridViewIcon from "@mui/icons-material/GridView";
@@ -18,7 +18,14 @@ import { SpaceBetween } from "@/components/styled";
 import FilterSortBy from "./SortBy";
 import FilterSection from "./FiltersSection";
 import { optionType } from "./types";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
+import { StyledPriceButton } from "@/components/Filters";
+
+import TuneIcon from "@mui/icons-material/Tune";
+
+import useDialog from "@/hooks/useDialog";
+import FilterMore from "./Filters/FilterMore";
+import useResponsive from "@/hooks/useResponsive";
 
 type viewOptionsType = {
     id: optionType;
@@ -64,6 +71,8 @@ const FilterBar = ({
 
     belowLg,
 }: Props) => {
+    const dispatch = useDispatch();
+
     const changedPropertyFilters = useSelector(sumOfChangedProperties);
 
     const BUTTONS = useMemo(() => {
@@ -82,41 +91,74 @@ const FilterBar = ({
         ));
     }, [optionView, belowLg]);
 
+    const handleResetFilter = useCallback(() => dispatch(resetState()), []);
+
+    const [isDialogOpen, openDialog, closeDialog] = useDialog();
+
+    const belowXl = useResponsive("down", "xl");
+
     return (
-        <Paper
-            sx={{
-                p: 1,
-            }}
-        >
-            <SpaceBetween>
-                <Box
-                    sx={{
-                        overflowX: "auto",
-                        overflowY: "hidden",
-                    }}
-                >
-                    <FilterSection />
-                </Box>
-
-                <Stack direction="row" spacing={0.3}>
-                    <FilterSortBy
-                        sorting={sorting}
-                        onSortingChange={onSortingChange}
-                    />
-
-                    <ButtonGroup
-                        size="small"
+        <>
+            <Paper
+                sx={{
+                    p: 1,
+                }}
+            >
+                <SpaceBetween>
+                    <Stack
+                        direction="row"
+                        spacing={0.3}
                         sx={{
-                            gap: 0.3,
+                            overflowX: "auto",
+                            overflowY: "hidden",
                         }}
                     >
-                        {BUTTONS}
-                    </ButtonGroup>
-                </Stack>
-            </SpaceBetween>
+                        {belowXl ? null : <FilterSection />}
 
-            {changedPropertyFilters > 0 ? <ChosenFilters /> : null}
-        </Paper>
+                        <StyledPriceButton
+                            open={false}
+                            disableRipple
+                            color="inherit"
+                            onClick={openDialog}
+                        >
+                            <Badge
+                                badgeContent={changedPropertyFilters}
+                                color="error"
+                            >
+                                <TuneIcon />
+                            </Badge>
+                        </StyledPriceButton>
+                    </Stack>
+
+                    <Stack direction="row" spacing={0.3}>
+                        <FilterSortBy
+                            sorting={sorting}
+                            onSortingChange={onSortingChange}
+                        />
+
+                        <ButtonGroup
+                            size="small"
+                            sx={{
+                                gap: 0.3,
+                            }}
+                        >
+                            {BUTTONS}
+                        </ButtonGroup>
+                    </Stack>
+                </SpaceBetween>
+
+                {changedPropertyFilters > 0 ? <ChosenFilters /> : null}
+            </Paper>
+
+            {isDialogOpen ? (
+                <FilterMore
+                    open={isDialogOpen}
+                    onOpen={openDialog}
+                    onClose={closeDialog}
+                    onResetFilter={handleResetFilter}
+                />
+            ) : null}
+        </>
     );
 };
 
--- src/pages/property/FiltersBar/FiltersSection.tsx
+++ src/pages/property/FiltersBar/FiltersSection.tsx
@@ -1,81 +1,28 @@
-import TuneIcon from "@mui/icons-material/Tune";
-import { Badge, Stack } from "@mui/material";
-import { useDispatch, useSelector } from "src/store";
+import { useSelector } from "src/store";
 // Filters
 import FilterParentCategory from "./Filters/ParentCategory";
 import FilterCategory from "./Filters/Category";
 import FilterLabels from "./Filters/Labels";
-import FilterMore from "./Filters/FilterMore";
 import PriceSelect from "./Filters/Price";
 import SaleSelect from "./Filters/Sale";
 
-import { StyledPriceButton } from "@/components/Filters";
-
-import {
-    selectLabels,
-    setLabels,
-    sumOfChangedProperties,
-    resetState,
-} from "src/slices/filters";
-
-import useDialog from "@/hooks/useDialog";
-import useResponsive from "@/hooks/useResponsive";
+import { selectLabels, setLabels } from "src/slices/filters";
 
 const FilterSection = () => {
-    const dispatch = useDispatch();
-
-    const changedPropertyFilters = useSelector(sumOfChangedProperties);
     const labels = useSelector(selectLabels) || [];
-
-    const handleResetFilter = () => {
-        dispatch(resetState());
-    };
-
-    const [isDialogOpen, openDialog, closeDialog] = useDialog();
-
-    const belowSm = useResponsive("down", "sm");
 
     return (
         <>
-            <Stack direction={"row"} minWidth={"350px"} flex={1} spacing={1}>
-                {belowSm ? null : (
-                    <>
-                        <SaleSelect />
-                        <FilterParentCategory />
-                        <FilterCategory />
-
-                        <PriceSelect type={"price"} />
-                        <PriceSelect type={"area"} />
-                        <FilterLabels
-                            variant="property"
-                            labels={labels}
-                            setLabels={setLabels}
-                        />
-                        <StyledPriceButton
-                            open={false}
-                            disableRipple
-                            color="inherit"
-                            onClick={openDialog}
-                        >
-                            <Badge
-                                badgeContent={changedPropertyFilters}
-                                color="error"
-                            >
-                                <TuneIcon />
-                            </Badge>
-                        </StyledPriceButton>
-                    </>
-                )}
-
-                {isDialogOpen ? (
-                    <FilterMore
-                        open={isDialogOpen}
-                        onOpen={openDialog}
-                        onClose={closeDialog}
-                        onResetFilter={handleResetFilter}
-                    />
-                ) : null}
-            </Stack>
+            <SaleSelect />
+            <FilterParentCategory />
+            <FilterCategory />
+            <PriceSelect type={"price"} />
+            <PriceSelect type={"area"} />
+            <FilterLabels
+                variant="property"
+                labels={labels}
+                setLabels={setLabels}
+            />
         </>
     );
 };
