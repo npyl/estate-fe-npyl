f3b94b5be1950b88af1e9567df65f792450de1f9
Mpooks <jimbalafas2001@gmail.com>
2024-05-21 16:21:36 +0300

    kapoies allages sta statistika giati den fainontan kala ta charts

--- src/pages/statistics/liveViewsChart.tsx
+++ src/pages/statistics/liveViewsChart.tsx
@@ -12,8 +12,6 @@ import { useMemo } from "react";
 import { Box, Stack } from "@mui/system";
 import { Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import { Live as LiveIcon } from "@/icons/live";
-import { SpaceBetween } from "@/components/styled";
 import { useGetDailyViewsQuery } from "@/services/publicDashboard";
 import { StyledCursor } from "./styled";
 
@@ -79,105 +77,106 @@ export default function ViewsChart() {
                     </Typography>
                 </Stack>
             </Stack>
-            <ResponsiveContainer height={300}>
-                <BarChart height={300} data={chartData}>
-                    <CartesianGrid vertical={false} />
-                    <XAxis
-                        dataKey="hour"
-                        tickFormatter={(tick) => {
-                            const date = new Date();
-                            date.setHours(tick);
-                            return date.toLocaleTimeString([], {
-                                hour: "2-digit",
-                                hour12: false,
-                            });
-                        }}
-                        interval={2}
-                    />
-                    <YAxis dataKey="views" width={20} />
-                    <Tooltip
-                        cursor={<StyledCursor />}
-                        content={({ payload }) => {
-                            if (payload?.length) {
-                                const date = new Date();
-                                date.setHours(payload[0].payload.hour);
-                                const timeString = date.toLocaleTimeString([], {
-                                    hour: "2-digit",
-                                    hour12: false,
-                                });
+            <Box style={{ display: "flex", justifyContent: "center" }}>
+                <ResponsiveContainer height={300}>
+                    <BarChart data={chartData} margin={{ left: 30, right: 30 }}>
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="hour"
+                            tickFormatter={(tick) => tick.toString()}
+                            interval={2}
+                        />
+                        <YAxis dataKey="views" width={20} />
+                        <Tooltip
+                            cursor={<StyledCursor />}
+                            content={({ payload }) => {
+                                if (payload?.length) {
+                                    const date = new Date();
+                                    date.setHours(payload[0].payload.hour);
+                                    const timeString = date.toLocaleTimeString(
+                                        [],
+                                        {
+                                            hour: "2-digit",
+                                            hour12: false,
+                                        }
+                                    );
 
-                                return (
-                                    <Box
-                                        sx={{
-                                            background: "white",
-                                            borderRadius: "5px",
-                                            boxShadow:
-                                                "0 0 10px rgba(0, 0, 0, 0.1)",
-                                            border: "1px solid #ccc",
-                                            color: "black",
-                                            justifyContent: "center",
-                                            alignItems: "center",
-                                            width: "auto",
-                                            height: "auto",
-                                            textAlign: "center",
-                                            paddingLeft: "5px",
-                                            paddingRight: "5px",
-                                            display: "flex",
-                                            flexDirection: "column",
-                                        }}
-                                    >
-                                        <p
-                                            style={{
-                                                margin: 0,
-                                                marginTop: "5px",
-                                            }}
-                                        >
-                                            <b>Time :</b> {timeString}:00
-                                        </p>
-                                        <hr
-                                            style={{
-                                                borderColor: "grey",
-                                                width: "100%",
-                                            }}
-                                        />
-                                        <div
-                                            style={{
-                                                display: "flex",
+                                    return (
+                                        <Box
+                                            sx={{
+                                                background: "white",
+                                                borderRadius: "5px",
+                                                boxShadow:
+                                                    "0 0 10px rgba(0, 0, 0, 0.1)",
+                                                border: "1px solid #ccc",
+                                                color: "black",
+                                                justifyContent: "center",
                                                 alignItems: "center",
+                                                width: "auto",
+                                                height: "auto",
+                                                textAlign: "center",
+                                                paddingLeft: "5px",
+                                                paddingRight: "5px",
+                                                display: "flex",
+                                                flexDirection: "column",
                                             }}
                                         >
-                                            <div
-                                                style={{
-                                                    width: "10px",
-                                                    height: "10px",
-                                                    borderRadius: "50%",
-                                                    backgroundColor: "#3366FF",
-                                                    marginRight: "5px",
-                                                }}
-                                            ></div>
                                             <p
                                                 style={{
                                                     margin: 0,
-                                                    marginBottom: "5px",
+                                                    marginTop: "5px",
                                                 }}
                                             >
-                                                <b>Views:</b> {payload[0].value}
+                                                <b>Time :</b> {timeString}
+                                                :00
                                             </p>
-                                        </div>
-                                    </Box>
-                                );
-                            }
-                            return null;
-                        }}
-                    />
-                    <Bar
-                        dataKey="views"
-                        fill="#3366FF"
-                        barSize={50}
-                        shape={<Rectangle radius={[5, 5, 0, 0]} />}
-                    />
-                </BarChart>
-            </ResponsiveContainer>
+                                            <hr
+                                                style={{
+                                                    borderColor: "#D3D3D3",
+                                                    width: "100%",
+                                                }}
+                                            />
+                                            <div
+                                                style={{
+                                                    display: "flex",
+                                                    alignItems: "center",
+                                                }}
+                                            >
+                                                <div
+                                                    style={{
+                                                        width: "10px",
+                                                        height: "10px",
+                                                        borderRadius: "50%",
+                                                        backgroundColor:
+                                                            "#3366FF",
+                                                        marginRight: "5px",
+                                                    }}
+                                                ></div>
+                                                <p
+                                                    style={{
+                                                        margin: 0,
+                                                        marginBottom: "5px",
+                                                    }}
+                                                >
+                                                    <b>Views:</b>{" "}
+                                                    {payload[0].value}
+                                                </p>
+                                            </div>
+                                        </Box>
+                                    );
+                                }
+                                return null;
+                            }}
+                        />
+                        <Bar
+                            dataKey="views"
+                            fill="#3366FF"
+                            barSize={50}
+                            shape={<Rectangle radius={[5, 5, 0, 0]} />}
+                        />
+                    </BarChart>
+                </ResponsiveContainer>
+            </Box>
         </>
     );
 }
