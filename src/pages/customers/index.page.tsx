363fb448987a7cbad704fd61f515f8c8402baeb7
ntinoskost800 <ntinos_kost@digipath.gr>
2024-06-21 09:21:08 +0300

    sticky FilterBar in customers page

--- src/pages/customers/index.page.tsx
+++ src/pages/customers/index.page.tsx
@@ -125,6 +125,15 @@ const Customers: NextPage = () => {
                 sx={{
                     marginRight: bulkEditOpen ? 40 : 0,
                     mb: 1,
+                    position: "sticky",
+                    top: 64,
+                    zIndex: 10,
+                    background: "rgba(255, 255, 255, 0.7)",
+                    boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+                    backdropFilter: "blur(8.5px)",
+                    borderRadius: "10px",
+                    border: "1px solid rgba(255, 255, 255, 0.18)",
+                    width: "100%",
                 }}
                 sorting={sorting}
                 onSortingChange={setSorting}
