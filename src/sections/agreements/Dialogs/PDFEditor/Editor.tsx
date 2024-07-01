0e934324f41cb9ad6f5f1eb8d8fd007384a4be00
npyl <pylarinosnick@gmail.com>
2024-07-01 17:03:23 +0300

    This makes things a bit less re-rendery.

--- src/sections/agreements/Dialogs/PDFEditor/Editor.tsx
+++ src/sections/agreements/Dialogs/PDFEditor/Editor.tsx
@@ -1,53 +1,10 @@
 import { useForm } from "./hook";
-import { useEffect, useRef, useState } from "react";
-import { Template } from "@pdfme/common";
-import { BasicSchema } from "./constants";
-import { IAgreementReq } from "@/types/agreements";
-import { useFormContext } from "react-hook-form";
-import { flattenObject } from "./util";
-
-export const getSampleTemplate = (basePdf: any): Template => ({
-    schemas: BasicSchema,
-    basePdf,
-});
+import { useRef } from "react";
 
 const PDFEditor = () => {
-    const { watch, setValue } = useFormContext();
-
-    const [template, setTemplate] = useState<Template | null>(null);
-
-    const all = watch() as IAgreementReq;
-    const { variant, lang } = all;
-
-    useEffect(() => {
-        const loadPdf = async () => {
-            try {
-                const res = await fetch("/api/pdf", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/pdf",
-                    },
-                    body: JSON.stringify({
-                        variant,
-                        lang,
-                    }),
-                });
-
-                const basePdf = await res.text();
-
-                const template = getSampleTemplate(basePdf);
-
-                setTemplate(template);
-            } catch (error) {
-                console.error("Error loading PDF:", error);
-            }
-        };
-        loadPdf();
-    }, []);
-
     const formRef = useRef<HTMLDivElement>(null);
 
-    useForm(formRef, template, [flattenObject(all)], setValue);
+    useForm(formRef);
 
     return <div ref={formRef} />;
 };
--- src/sections/agreements/Dialogs/PDFEditor/hook.ts
+++ src/sections/agreements/Dialogs/PDFEditor/hook.ts
@@ -1,40 +1,71 @@
 import { useEffect, useRef } from "react";
 import type { Template } from "@pdfme/common";
 import type { Form } from "@pdfme/ui";
+import { BasicSchema } from "./constants";
+import { useFormContext } from "react-hook-form";
+import { IAgreementReq } from "@/types/agreements";
+import { flattenObject } from "./util";
 
-type Input = { [key: string]: any };
+export const getSampleTemplate = (basePdf: any): Template => ({
+    schemas: BasicSchema,
+    basePdf,
+});
 
 export const useForm = (
-    formRef: React.MutableRefObject<HTMLDivElement | null>,
-    template: Template | null,
-    inputs: Input[] | undefined,
-    onChangeInput: (key: string, value: any) => void
+    formRef: React.MutableRefObject<HTMLDivElement | null>
 ) => {
+    const { watch, setValue } = useFormContext();
+    const all = watch() as IAgreementReq;
+    const { variant, lang } = all;
+
     const form = useRef<Form | null>(null);
 
     useEffect(() => {
-        if (!template) return;
-
-        if (formRef.current && form.current === null) {
-            import("@pdfme/ui").then(({ Form }) => {
-                form.current = new Form({
-                    domContainer: formRef.current!!,
-                    template,
-                    inputs: inputs || [{}],
-                    // ...
-                    // plugins: { text, image, ...barcodes },
-                    // options: { font },
+        const loadPdf = async () => {
+            try {
+                const res = await fetch("/api/pdf", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/pdf",
+                    },
+                    body: JSON.stringify({
+                        variant,
+                        lang,
+                    }),
                 });
 
-                form.current.onChangeInput(({ key, value }) =>
-                    onChangeInput(key, value)
-                );
-            });
-        } else if (form.current) {
-            form.current?.updateTemplate(template);
-            form.current.setInputs(inputs || [{}]);
-        }
-    }, [template, inputs]);
+                const basePdf = await res.text();
+
+                const template = getSampleTemplate(basePdf);
+
+                return template;
+            } catch (error) {
+                console.error("Error loading PDF:", error);
+                return null;
+            }
+        };
+
+        loadPdf().then((template) => {
+            if (formRef.current && form.current === null && !!template) {
+                import("@pdfme/ui").then(({ Form }) => {
+                    const inputs = [flattenObject(all)];
+
+                    form.current = new Form({
+                        domContainer: formRef.current!!,
+                        template,
+                        inputs: inputs || [{}],
+                        // ...
+                        // plugins: { text, image, ...barcodes },
+                        // options: { font },
+                    });
+
+                    form.current.onChangeInput(({ key, value }) =>
+                        setValue(key, value)
+                    );
+                });
+            }
+        });
+    }, []);
 
     return form.current;
 };
