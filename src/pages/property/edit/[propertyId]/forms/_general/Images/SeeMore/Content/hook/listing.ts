0301e8ecaf459ea45d52a66669f4b7e70b545d85
npyl <pylarinosnick@gmail.com>
2024-07-19 16:19:00 +0300

    Public/Private

--- src/pages/property/edit/[propertyId]/forms/_general/Images/SeeMore/Content/hook/listing.ts
+++ src/pages/property/edit/[propertyId]/forms/_general/Images/SeeMore/Content/hook/listing.ts
@@ -16,31 +16,42 @@ const useListingContentOperations: TUseContentOperations = (
 ) => {
     const { images, propertyId } = usePropertyImages();
 
-    const { data } = useGetIntegrationOrderedImagesQuery({
-        integrationSite: tab as IntegrationSite,
-        propertyId,
-    });
-
-    const { publicImages, privateImages } = useMemo(
-        () => ({
-            publicImages:
-                data
-                    ?.map(({ image: { id: _id } }) =>
-                        images.find(({ id }) => id === _id)
-                    )
-                    .filter((img) => !!img)
-                    .map(createItemCb) || [],
-
-            privateImages:
-                data
-                    ?.map(({ image: { id: _id } }) =>
-                        images.find(({ id }) => id !== _id)
-                    )
-                    .filter((img) => !!img)
-                    .map(createItemCb) || [],
-        }),
-        [images, createItemCb]
+    const { data } = useGetIntegrationOrderedImagesQuery(
+        {
+            integrationSite: tab as IntegrationSite,
+            propertyId,
+        },
+        { skip: tab === "CRM" }
     );
+
+    const { publicImages, privateImages } = useMemo(() => {
+        const tmp =
+            data
+                ?.map(({ image: { id: _id } }) =>
+                    images.find(({ id }) => id === _id)
+                )
+                .filter((img) => !!img) || [];
+
+        // all images that come from the endpoint are in the first section
+        const publicImages = tmp.map(createItemCb) || [];
+
+        // all images that do not are supposed to be hidden from the integration
+        const privateImages =
+            images
+                ?.filter(
+                    ({ id }) =>
+                        data?.findIndex(
+                            ({ image: { id: _id } }) => id === _id
+                        ) === -1
+                )
+                .filter((img) => !!img)
+                .map(createItemCb) || [];
+
+        return {
+            publicImages,
+            privateImages,
+        };
+    }, [images, data, createItemCb]);
 
     const handleDragEnd = useCallback(
         ({ type, draggableId, source, destination }: DropResult) => {},
