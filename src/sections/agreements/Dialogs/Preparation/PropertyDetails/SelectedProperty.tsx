2151994e1e4c9162300df8061a73286860f52119
npyl <pylarinosnick@gmail.com>
2024-08-01 20:50:25 +0300

    ok.

--- src/sections/agreements/Dialogs/Preparation/PropertyDetails/SelectedProperty.tsx
+++ src/sections/agreements/Dialogs/Preparation/PropertyDetails/SelectedProperty.tsx
@@ -2,6 +2,7 @@ import { useGetPropertyByIdQuery } from "@/services/properties";
 import Skeleton from "@mui/material/Skeleton";
 import { useFormContext } from "react-hook-form";
 import Typography from "@mui/material/Typography";
+import { useTranslation } from "react-i18next";
 
 const useSelectedProperty = () => {
     const { watch } = useFormContext();
@@ -20,6 +21,7 @@ const useSelectedProperty = () => {
 // ----------------------------------------------------------------------
 
 const SelectedProperty = () => {
+    const { t } = useTranslation();
     const { property, isLoading } = useSelectedProperty();
 
     if (!property) return null;
@@ -27,7 +29,11 @@ const SelectedProperty = () => {
     if (isLoading)
         return <Skeleton width="100%" height={40} animation="pulse" />;
 
-    return <Typography>Property: {property?.code}</Typography>;
+    return (
+        <Typography>
+            {t("Selected Property")}: {property?.code}
+        </Typography>
+    );
 };
 
 export default SelectedProperty;
--- src/sections/agreements/Dialogs/Preparation/styled.ts
+++ src/sections/agreements/Dialogs/Preparation/styled.ts
@@ -1,3 +1,4 @@
+import getBorderColor from "@/theme/borderColor";
 import DialogActions from "@mui/material/DialogActions";
 import { styled } from "@mui/material/styles";
 
@@ -5,6 +6,9 @@ const StyledActions = styled(DialogActions)(({ theme }) => ({
     display: "flex",
     flexDirection: "row",
     gap: theme.spacing(1),
+    // ...
+    borderTop: "1px solid",
+    borderTopColor: getBorderColor(theme),
 }));
 
 export { StyledActions };
