15849a42bb2e146b0f8fe0a7265f2c01316efa05
npyl <pylarinosnick@gmail.com>
2024-08-09 22:00:55 +0300

    drop formik

--- src/components/authentication/jwt-login.tsx
+++ src/components/authentication/jwt-login.tsx
@@ -1,16 +1,14 @@
-import {
-    Button,
-    FormHelperText,
-    TextField as MuiTextField,
-} from "@mui/material";
+import { Button, TextField as MuiTextField } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { useFormik } from "formik";
 import { useRouter } from "next/router";
-import type { FC } from "react";
+import { useMemo, type FC } from "react";
 import * as Yup from "yup";
 import { useAuth } from "../../hooks/use-auth";
-import { useMounted } from "../../hooks/use-mounted";
 import { useTranslation } from "react-i18next";
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { TranslationType } from "@/types/translation";
+import { RHFTextField } from "../hook-form";
 
 const Form = styled("form")(({ theme }) => ({
     display: "flex",
@@ -22,89 +20,74 @@ const TextField = styled(MuiTextField)({
     margin: 0,
 });
 
-export const JWTLogin: FC = (props) => {
-    const { t } = useTranslation();
-    const router = useRouter();
-    const isMounted = useMounted();
-    const { signin } = useAuth();
-
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: "",
-            submit: null,
-        },
-        validationSchema: Yup.object({
-            email: Yup.string()
-                .email(t("Must be a valid email") || "")
-                .max(255)
-                .required(t("Email is required") || ""),
-            password: Yup.string()
-                .max(255)
-                .required(t("Password is required") || ""),
-        }),
-        onSubmit: async (values, helpers): Promise<void> => {
-            try {
-                await signin(values.email, values.password);
-
-                if (isMounted()) {
-                    const returnUrl =
-                        (router.query.returnUrl as string | undefined) || "/";
-                    router.push(returnUrl).catch(console.error);
-                }
-            } catch (err) {
-                console.error(err);
-
-                if (isMounted()) {
-                    helpers.setStatus({ success: false });
-                    helpers.setErrors({ submit: err.message });
-                    helpers.setSubmitting(false);
-                }
-            }
-        },
+const getSchema = (t: TranslationType) =>
+    Yup.object({
+        email: Yup.string()
+            .email(t("Must be a valid email") || "")
+            .max(255)
+            .required(t("Email is required") || ""),
+        password: Yup.string()
+            .max(255)
+            .required(t("Password is required") || ""),
     });
 
+export const JWTLogin: FC = (props) => {
+    const { t } = useTranslation();
+
+    const schema = useMemo(() => getSchema(t), [t]);
+
+    const router = useRouter();
+
+    const { signin } = useAuth();
+
+    const methods = useForm({
+        values: {
+            email: "",
+            password: "",
+        },
+        resolver: yupResolver(schema),
+    });
+
+    const handleSubmit = async (values: any) => {
+        try {
+            await signin(values.email, values.password);
+
+            const returnUrl =
+                (router.query.returnUrl as string | undefined) || "/";
+            router.push(returnUrl).catch(console.error);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
-        <Form noValidate onSubmit={formik.handleSubmit} {...props}>
-            <TextField
-                autoFocus
-                error={Boolean(formik.touched.email && formik.errors.email)}
-                fullWidth
-                helperText={formik.touched.email && formik.errors.email}
-                placeholder={t("Email Address") || ""}
-                margin="normal"
-                name="email"
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                type="email"
-                value={formik.values.email}
-            />
-            <TextField
-                error={Boolean(
-                    formik.touched.password && formik.errors.password
-                )}
-                fullWidth
-                helperText={formik.touched.password && formik.errors.password}
-                placeholder={t("Password") || ""}
-                margin="normal"
-                name="password"
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                type="password"
-                value={formik.values.password}
-            />
-            {formik.errors.submit && (
-                <FormHelperText error>{formik.errors.submit}</FormHelperText>
-            )}
-            <Button
-                disabled={formik.isSubmitting}
-                fullWidth
-                size="large"
-                type="submit"
-                variant="contained"
-            >
-                {t("Log In")}
-            </Button>
-        </Form>
+        <FormProvider {...methods}>
+            <Form onSubmit={methods.handleSubmit(handleSubmit)} {...props}>
+                <RHFTextField
+                    autoFocus
+                    fullWidth
+                    placeholder={t("Email Address") || ""}
+                    margin="normal"
+                    name="email"
+                    type="email"
+                />
+                <TextField
+                    fullWidth
+                    placeholder={t("Password") || ""}
+                    margin="normal"
+                    name="password"
+                    type="password"
+                />
+                <Button
+                    disabled={methods.formState.isSubmitting}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                >
+                    {t("Log In")}
+                </Button>
+            </Form>
+        </FormProvider>
     );
 };
