0391cde9bacdce2c474cdeff5b30962dac2548e4
ntinoskost800 <ntinos_kost@digipath.gr>
2024-09-13 17:19:59 +0300

    translation fixed in chosen Filters in customer and property pages

--- src/pages/customers/(FilterSection)/Filters/ChosenFilters.tsx
+++ src/pages/customers/(FilterSection)/Filters/ChosenFilters.tsx
@@ -203,15 +203,16 @@ const ChosenFilters = (props: GridProps) => {
                                             paddingRight: 1,
                                         }}
                                     >
-                                        {label}
-                                        {isRole ? "" : ":"}
+                                        {label}:{/* {isRole ? "" : ":"} */}
                                     </Typography>
                                     <Typography
                                         sx={{ textTransform: "lowercase" }}
                                     >
                                         {Array.isArray(valuesToDisplay)
-                                            ? valuesToDisplay.join(", ")
-                                            : valuesToDisplay}
+                                            ? valuesToDisplay
+                                                  .map((value) => t(value))
+                                                  .join(", ")
+                                            : t(valuesToDisplay)}
                                     </Typography>
                                 </Stack>
                             }
