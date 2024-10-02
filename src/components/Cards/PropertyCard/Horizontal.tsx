e19195c4994b5a919386efe63116d768c7bf92b5
npyl <pylarinosnick@gmail.com>
2024-10-02 15:57:13 +0300

    kostas: darkmode 2/2

--- src/components/Cards/PropertyCard/Horizontal.tsx
+++ src/components/Cards/PropertyCard/Horizontal.tsx
@@ -223,7 +223,13 @@ const PropertyCard = ({ item, selectedMarker }: PropertyCardProps) => {
                             <NormalBadge
                                 name={`${t("Code")}: ${code || ""}`}
                                 color="#ffcc00"
-                                sx={{ color: "#854D0E" }}
+                                sx={{
+                                    color: (theme) =>
+                                        theme.palette.mode === "light"
+                                            ? "#854D0E" ||
+                                              theme.palette.grey[700] // Fallback to grey if neutral is undefined
+                                            : "null",
+                                }}
                             />
 
                             <PriceBadge price={price} />
