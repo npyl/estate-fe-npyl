24dfa844041a49d5121b57d970a6904b18ecfb9a
npyl <pylarinosnick@gmail.com>
2024-10-02 19:00:52 +0300

    cities.

--- src/pages/property/(FiltersBar)/ChosenFilters/Chips/Cities.tsx
+++ src/pages/property/(FiltersBar)/ChosenFilters/Chips/Cities.tsx
@@ -1,42 +1,45 @@
 import { useLazyGetMunicipalitiesQuery } from "@/services/location";
-import { selectCities } from "@/slices/filters";
+import { selectCities, selectRegions } from "@/slices/filters";
 import Chip from "@mui/material/Chip";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import ChipLabel from "./ChipLabel";
 import { IGeoLocation } from "@/types/geolocation";
 
-const Cities = () => {
-    const { t, i18n } = useTranslation();
-
-    const [getMunicips] = useLazyGetMunicipalitiesQuery();
-
-    const areaIDs = useSelector(selectCities);
-
-    const [names, setNames] = useState("");
-
-    const getNames = async (
-        promises: Promise<IGeoLocation[]>[],
-        lng: string
-    ) => {
-        const res = await Promise.all(promises);
-
-        const names = res
-            .flat()
+const getNames = async (
+    promises: Promise<IGeoLocation[]>[],
+    cityIDs: string[],
+    lng: string
+) => {
+    try {
+        const allCities = (await Promise.all(promises)).flat();
+        const filteredCities = allCities.filter(({ areaID }) =>
+            cityIDs.includes(areaID.toString())
+        );
+        const names = filteredCities
             .map(({ nameEN, nameGR }) => (lng === "en" ? nameEN : nameGR))
             .join(", ");
 
-        console.log("GOT: ", names);
+        return names;
+    } catch (ex) {
+        return "";
+    }
+};
 
-        setNames(names);
-    };
+const Cities = () => {
+    const { t, i18n } = useTranslation();
+    const [getMunicips] = useLazyGetMunicipalitiesQuery();
 
-    // re-call getNames() when areaIDs change
+    const regionIDs = useSelector(selectRegions) || [];
+    const cityIDs = useSelector(selectCities) || [];
+
+    const [names, setNames] = useState("");
+
     useEffect(() => {
-        const promises = areaIDs.map((i) => getMunicips(+i).unwrap());
-        getNames(promises, i18n.language);
-    }, [areaIDs, i18n.language]);
+        const promises = regionIDs.map((id) => getMunicips(+id).unwrap());
+        getNames(promises, cityIDs, i18n.language).then(setNames);
+    }, [regionIDs, cityIDs, i18n.language]);
 
     return <Chip label={<ChipLabel title={t("Cities")} value={names} />} />;
 };
