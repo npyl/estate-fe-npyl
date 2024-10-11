1ba413d47eadb7876b8cbf9e54de51ddb24291c0
npyl <pylarinosnick@gmail.com>
2024-10-11 15:19:25 +0300

    Update header

--- src/pages/dashboard/Calendar/Header.tsx
+++ src/pages/dashboard/Calendar/Header.tsx
@@ -1,10 +1,48 @@
 import BaseHeader from "@/components/BaseCalendar/Header";
-import { BaseCalendarHeaderProps } from "@/components/BaseCalendar/types";
+import {
+    BaseCalendarHeaderProps,
+    TCalendarView,
+} from "@/components/BaseCalendar/types";
 import Typography from "@mui/material/Typography";
 import { CSSProperties, FC } from "react";
 
 const Style: CSSProperties = {
     position: "relative",
+};
+
+const renderValue = (view: TCalendarView, date: Date): string => {
+    const options: Intl.DateTimeFormatOptions = { timeZone: "UTC" };
+
+    switch (view) {
+        case "day":
+            options.weekday = "long";
+            options.year = "numeric";
+            options.month = "long";
+            options.day = "numeric";
+            break;
+        case "week":
+            const startOfWeek = new Date(date);
+            startOfWeek.setDate(date.getDate() - date.getDay());
+            const endOfWeek = new Date(startOfWeek);
+            endOfWeek.setDate(startOfWeek.getDate() + 6);
+            return `${startOfWeek.toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+            })} - ${endOfWeek.toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+                year: "numeric",
+            })}`;
+        case "month":
+            options.year = "numeric";
+            options.month = "long";
+            break;
+        case "year":
+            options.year = "numeric";
+            break;
+    }
+
+    return date.toLocaleDateString("en-US", options);
 };
 
 const CalendarHeader: FC<BaseCalendarHeaderProps> = ({
@@ -20,7 +58,7 @@ const CalendarHeader: FC<BaseCalendarHeaderProps> = ({
                 transform: "translate(-50%, -50%)",
             }}
         >
-            {props.date.toDateString()}
+            {renderValue(props.view, props.date)}
         </Typography>
     </BaseHeader>
 );
