1b96524651caa7da25541b4ba2ccc76425d4439c
npyl <npylarinos@digipath.gr>
2024-10-18 17:06:20 +0300

    minor fix.

--- src/components/BaseCalendar/util.ts
+++ src/components/BaseCalendar/util.ts
@@ -1,12 +1,14 @@
 const getStartOfWeek = (date: Date) => {
     const result = new Date(date);
     result.setDate(date.getDate() - date.getDay());
+    result.setHours(0, 0, 0, 0);
     return result;
 };
 
 const getEndOfWeek = (date: Date) => {
     const result = new Date(date);
     result.setDate(date.getDate() - date.getDay() + 6);
+    result.setHours(23, 59, 59, 999);
     return result;
 };
 
