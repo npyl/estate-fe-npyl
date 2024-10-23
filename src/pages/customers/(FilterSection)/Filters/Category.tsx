41827028d1ad100fbbe9cfa1299a93ca24c99026
ntinoskost800 <ntinos_kost@digipath.gr>
2024-10-23 10:53:27 +0300

    .

--- src/pages/customers/(FilterSection)/Filters/Category.tsx
+++ src/pages/customers/(FilterSection)/Filters/Category.tsx
@@ -73,7 +73,7 @@ export default function FilterCategory() {
                                     parentCategory
                                 ].find((item) => item.key === key);
                                 if (found) {
-                                    return found.value;
+                                    return t(found.value);
                                 }
                             }
                             return null;
