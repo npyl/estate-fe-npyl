cd402246f3cb69297d5e0ec20aff63ceb1f97856
npyl <npylarinos@digipath.gr>
2024-10-27 20:06:31 +0200

    search: open event dialog on event click

--- src/sections/Calendar/Filters/Search.tsx
+++ src/sections/Calendar/Filters/Search.tsx
@@ -13,12 +13,14 @@ import MuiPopover from "@mui/material/Popover";
 import { TCalendarEvent } from "@/components/Calendar/types";
 import CompactCalendarEvent from "@/components/Calendar/Event/Compact";
 import Stack from "@mui/material/Stack";
+import dynamic from "next/dynamic";
+const EventDialog = dynamic(() => import("../Event/View"));
 
 // --------------------------------------------------------------------
 
-const getEvent = (event: TCalendarEvent) => (
-    <CompactCalendarEvent key={event.id} event={event} />
-);
+const getEvent =
+    (onClick: (event: TCalendarEvent) => void) => (event: TCalendarEvent) =>
+        <CompactCalendarEvent key={event.id} event={event} onClick={onClick} />;
 
 interface PopoverProps {
     anchorEl: HTMLElement;
@@ -26,21 +28,30 @@ interface PopoverProps {
     onClose: VoidFunction;
 }
 
-const Popover: FC<PopoverProps> = ({ anchorEl, events, onClose }) => (
-    <MuiPopover
-        open
-        anchorEl={anchorEl}
-        anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-        }}
-        onClose={onClose}
-    >
-        <Stack spacing={1} width="max-content" p={1}>
-            {events.map(getEvent)}
-        </Stack>
-    </MuiPopover>
-);
+const Popover: FC<PopoverProps> = ({ anchorEl, events, onClose }) => {
+    const [event, setEvent] = useState<TCalendarEvent>();
+    const closeDialog = () => setEvent(undefined);
+
+    return (
+        <>
+            <MuiPopover
+                open
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                }}
+                onClose={onClose}
+            >
+                <Stack spacing={1} width="max-content" p={1}>
+                    {events.map(getEvent(setEvent))}
+                </Stack>
+            </MuiPopover>
+
+            {event ? <EventDialog event={event} onClose={closeDialog} /> : null}
+        </>
+    );
+};
 
 // --------------------------------------------------------------------
 
