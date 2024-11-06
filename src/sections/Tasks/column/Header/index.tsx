eaebc713cdaf0b2271b9693c9c2293a8cd5350ae
npyl <npylarinos@digipath.gr>
2024-11-06 13:04:34 +0200

    Improve overflowing for column header

--- src/sections/Tasks/column/Header/index.tsx
+++ src/sections/Tasks/column/Header/index.tsx
@@ -1,5 +1,5 @@
 import { SpaceBetween } from "@/components/styled";
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import dynamic from "next/dynamic";
 const Controls = dynamic(() => import("./Controls"));
 
@@ -12,9 +12,24 @@ type Props = {
 export default function Header({ name, count, columnId }: Props) {
     return (
         <SpaceBetween alignItems="center" spacing={1}>
-            <Typography variant="h6">
-                {name} ({count})
-            </Typography>
+            <Stack
+                direction="row"
+                spacing={1}
+                alignItems="center"
+                overflow="hidden"
+            >
+                <Typography
+                    variant="h6"
+                    overflow="hidden"
+                    textOverflow="ellipsis"
+                    maxWidth="80%"
+                >
+                    {name}
+                </Typography>
+                <Typography variant="h6" noWrap>
+                    ({count})
+                </Typography>
+            </Stack>
 
             <Controls columnId={columnId} />
         </SpaceBetween>
