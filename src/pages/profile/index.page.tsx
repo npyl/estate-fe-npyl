c64b2c929dd533a182898000964a429f9a536f8f
npyl <npylarinos@digipath.gr>
2024-11-13 11:12:43 +0200

    cleanup profile from tabs.

--- src/pages/profile/index.page.tsx
+++ src/pages/profile/index.page.tsx
@@ -1,13 +1,12 @@
-import { Container, Tabs, Tab } from "@mui/material";
+import { Container } from "@mui/material";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import ViewUser from "@/sections/User/View";
 import { DashboardLayout } from "src/components/dashboard/dashboard-layout";
 import { SecurityProvider } from "src/contexts/security";
 import { useTabsContext } from "src/contexts/tabs";
 import { useGetUserQuery } from "src/services/user";
-import TabPanel from "@/components/Tabs";
 
 import { AuthGuard } from "@/components/authentication/auth-guard";
 
@@ -18,7 +17,6 @@ const Profile: NextPage = () => {
     const { user } = useAuth();
     const { data: profile } = useGetUserQuery(+user?.id!);
     const { pushTab } = useTabsContext();
-    const [tab, setTab] = useState(0);
 
     useEffect(() => {
         if (profile) {
@@ -30,16 +28,11 @@ const Profile: NextPage = () => {
         }
     }, [profile, t, pushTab]);
 
-    const handleTabChange = (_: any, v: number) => setTab(v);
+    if (!profile) return null;
 
     return (
         <Container maxWidth="md">
-            <Tabs value={tab} onChange={handleTabChange}>
-                <Tab label={t("User Profile")} value={0} />
-            </Tabs>
-            <TabPanel value={tab} index={0}>
-                {profile ? <ViewUser user={profile} /> : null}
-            </TabPanel>
+            <ViewUser user={profile} />
         </Container>
     );
 };
