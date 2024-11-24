1484a86c7da48b08ed1edfd0402faaa27489be66
npyl <pylarinosnick@gmail.com>
2024-11-24 15:41:12 +0200

    Prefer to show pp-users in calendar's UserSelect instead of gw-users

--- src/sections/Calendar/Filters/UserSelect/index.tsx
+++ src/sections/Calendar/Filters/UserSelect/index.tsx
@@ -1,10 +1,13 @@
-import { useGetUsersQuery } from "@/services/calendar";
 import { useFiltersContext } from "../context";
 import Skeleton from "@mui/material/Skeleton";
 import dynamic from "next/dynamic";
 import useIsOfficeAdmin from "@/sections/Google/useIsOfficeAdmin";
 import AllButton from "./AllButton";
 import ClearButton from "./ClearButton";
+import { useAllUsersQuery } from "@/services/user";
+import { useMemo } from "react";
+import { IUser } from "@/types/user";
+const MoreAvatars = dynamic(() => import("./MoreAvatars"));
 
 const Loader = () => <Skeleton width="100px" height="58px" />;
 
@@ -16,14 +19,39 @@ const AvatarSelectGroup = dynamic(
 );
 
 /**
+ * Select all pp-users that have google workspace email assigned to them
+ */
+export const useOfficeUsers = (gwIsAdmin: boolean = false) => {
+    const { data, isLoading } = useAllUsersQuery(undefined, {
+        skip: !gwIsAdmin,
+    });
+
+    const officeUsers = useMemo(() => {
+        if (!data?.length) return [];
+
+        return data.reduce<IUser[]>((acc, user) => {
+            if (!user.workspaceEmail) {
+                return acc;
+            }
+
+            acc.push({
+                ...user,
+                id: user.workspaceEmail as any,
+            });
+
+            return acc;
+        }, []);
+    }, [data]);
+
+    return { officeUsers, isLoading };
+};
+/**
  * UserSelect makes sence only if the user is a Google Workspace admin!
  */
 const UserSelect = () => {
-    const { gwIsAdmin, gwUser, isChecking, userId } = useIsOfficeAdmin();
+    const { gwIsAdmin, gwUser, isChecking } = useIsOfficeAdmin();
 
-    const { data: officeUsers, isLoading } = useGetUsersQuery(userId!, {
-        skip: !gwIsAdmin,
-    });
+    const { officeUsers, isLoading } = useOfficeUsers(gwIsAdmin);
 
     const { calendarId, setCalendarId } = useFiltersContext();
 
@@ -41,7 +69,9 @@ const UserSelect = () => {
         <AvatarSelectGroup
             users={officeUsers}
             value={value}
+            max={6}
             onChange={setCalendarId}
+            MoreAvatars={MoreAvatars}
         >
             <AllButton />
             {calendarId && calendarId !== gwUser?.id ? <ClearButton /> : null}
