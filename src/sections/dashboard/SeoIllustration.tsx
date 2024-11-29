3925766831aa4564a46a8ff44d93f3cafd1d7962
npyl <npylarinos@digipath.gr>
2024-11-29 12:17:44 +0200

    .

--- src/sections/dashboard/SeoIllustration.tsx
+++ src/sections/dashboard/SeoIllustration.tsx
@@ -239,13 +239,6 @@ function SeoIllustration({ ...other }: BoxProps) {
                         <stop offset="1" stopColor={PRIMARY_DARK} />
                     </linearGradient>
                 </defs>
-
-                <image
-                    href="/assets/illustrations/characters/character_3.png"
-                    height="300"
-                    x="312"
-                    y="30"
-                />
             </svg>
         </Box>
     );
