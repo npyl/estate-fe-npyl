a02178ac9a0e3eeeee212afb942fb558a6b385a9
joanplex <joanplex@gmail.com>
2024-12-02 13:38:58 +0200

    Cleanup global

--- src/hooks/useGlobals.ts
+++ src/hooks/useGlobals.ts
@@ -1,9 +1,6 @@
-import { useTranslation } from "react-i18next";
 import { useAllGlobalsQuery } from "src/services/global";
 
 export const useGlobals = () => {
-    const { i18n } = useTranslation();
-    const language = i18n.language === "el" ? "el" : i18n.language;
-    const { data: result } = useAllGlobalsQuery(language);
+    const { data: result } = useAllGlobalsQuery();
     return result;
 };
