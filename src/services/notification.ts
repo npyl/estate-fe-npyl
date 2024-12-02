36b616f10b430e3e55e1d78d9fefee9a75f1e14d
joanplex <joanplex@gmail.com>
2024-12-02 14:16:04 +0200

    Notifications.

--- src/services/notification.ts
+++ src/services/notification.ts
@@ -4,11 +4,12 @@ import {
     NotViewedContactNotifications,
 } from "@/types/notification/notification";
 import IPage from "@/types/page";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {
     ContactNotification,
     ContactNotificationExtended,
 } from "src/types/notification";
+import { apiWithTranslation, createLanguageAwareHook as la } from "./_util";
 
 interface INotificationFilterParams {
     filter: INotificationFilter;
@@ -18,42 +19,18 @@ interface INotificationFilterParams {
     direction: string; // asc - desc
 }
 
-export const notification = createApi({
+export const notification = apiWithTranslation({
     reducerPath: "contactNotification",
     baseQuery: fetchBaseQuery({
         baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/contact/notification`,
-        prepareHeaders: (headers) => {
-            // By default, if we have a token in the store, let's use that for authenticated requests
-
-            headers.set(
-                "Authorization",
-                `Bearer  ${localStorage.getItem("accessToken")}`
-            );
-            headers.set(
-                "Accept-Language",
-                `${localStorage.getItem("language") ?? "el"}`
-            );
-
-            return headers;
-        },
     }),
     tagTypes: ["Notifications", "NotificationById"],
     endpoints: (builder) => ({
-        getNotifications: builder.query<ContactNotification[], void>({
-            query: () => ({
-                url: "",
-            }),
-
-            providesTags: ["Notifications"],
-        }),
         getNonViewedNotificationsCount: builder.query<
             NotViewedContactNotifications,
             void
         >({
-            query: () => ({
-                url: "non-viewed/count",
-            }),
-
+            query: () => "non-viewed/count",
             providesTags: ["Notifications"],
         }),
 
@@ -108,12 +85,18 @@ export const notification = createApi({
 });
 
 export const {
-    useGetNotificationsQuery,
-    useGetNotificationByIdQuery,
     useGetNonViewedNotificationsCountQuery,
-    useFilterNotificationsQuery,
     useToggleNotificationViewedStatusMutation,
     useDeleteNotificationMutation,
 
     useActiveAssignedTasksCountQuery,
 } = notification;
+
+const useFilterNotificationsQuery = la(
+    notification.useFilterNotificationsQuery
+);
+const useGetNotificationByIdQuery = la(
+    notification.useGetNotificationByIdQuery
+);
+
+export { useFilterNotificationsQuery, useGetNotificationByIdQuery };
