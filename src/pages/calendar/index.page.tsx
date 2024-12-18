2366f6c15d1af1242f45492b8463e4612aae6a52
npyl <npylarinos@digipath.gr>
2024-12-18 16:26:42 +0200

    disable ssr for the whole calendar to fix build (temporarily)!

--- src/pages/calendar/index.page.tsx
+++ src/pages/calendar/index.page.tsx
@@ -1,7 +1,10 @@
 import { AuthGuard } from "@/components/authentication/auth-guard";
 import { DashboardLayout } from "@/components/dashboard/dashboard-layout";
-import CalendarSection from "@/sections/Calendar";
+const CalendarSection = dynamic(() => import("@/sections/Calendar"), {
+    ssr: false,
+});
 import { NextPage } from "next";
+import dynamic from "next/dynamic";
 
 const CalendarPage: NextPage = () => {
     return <CalendarSection />;
