3b80b84d1649a95ff518a17baf3506bf2506751d
npyl <npylarinos@digipath.gr>
2025-01-05 23:19:50 +0200

    Drop unused slice

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -25,7 +25,6 @@ import { googleWorkspaceApi } from "@/services/company";
 import { googleOAuth } from "@/services/google-oauth";
 
 import { reducer as customerFiltersReducer } from "../slices/customer/filters";
-import { reducer as customerMiscReducer } from "../slices/customer/misc";
 import { reducer as filtersReducer } from "../slices/filters";
 import { reducer as logReducer } from "../slices/log";
 import { reducer as securityReducer } from "../slices/security";
@@ -63,7 +62,6 @@ export const rootReducer = combineReducers({
     // property
     filters: filtersReducer,
     // customer
-    customerMisc: customerMiscReducer,
     customerFilters: customerFiltersReducer,
     // general
     securitySlice: securityReducer,
