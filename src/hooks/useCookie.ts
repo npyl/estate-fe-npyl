4067d44154ef1718c74872a5da966b283dfbd987
npyl <npylarinos@digipath.gr>
2025-02-05 09:56:06 +0200

    Experimental: try to fix cookies on deployed.

--- src/hooks/useCookie.ts
+++ src/hooks/useCookie.ts
@@ -1,6 +1,18 @@
 import { useCallback } from "react";
 import { useCookies } from "react-cookie";
 
+const LIBRARY_OPTIONS = {
+    doNotParse: false /* Make sure we always use JSON parser */,
+};
+
+// TODO: when we get to HTTPS this should be generalised to secure & strict
+const isSecure = process.env.NEXT_PUBLIC_SAFE_COOKIES === "1";
+
+const OPTIONS = {
+    sameSite: isSecure ? "strict" : "lax",
+    secure: isSecure,
+} as const;
+
 /**
  * Single cookie (to be used inside the respective context)
  * @param cookieName the cookie key
@@ -11,12 +23,13 @@ const useCookie = <V extends string | number | object = string>(
     cookieName: string,
     fallbackValue: V
 ) => {
-    const [value, set, remove] = useCookies<string, { [K in string]: V }>([
-        cookieName,
-    ]);
+    const [value, set, remove] = useCookies<string, { [K in string]: V }>(
+        [cookieName],
+        LIBRARY_OPTIONS
+    );
 
-    const actualSet = useCallback((v: V) => set(cookieName, v), []);
-    const actualRemove = useCallback(() => remove(cookieName), []);
+    const actualSet = useCallback((v: V) => set(cookieName, v, OPTIONS), []);
+    const actualRemove = useCallback(() => remove(cookieName, OPTIONS), []);
 
     const res = value?.[cookieName] || fallbackValue;
 
