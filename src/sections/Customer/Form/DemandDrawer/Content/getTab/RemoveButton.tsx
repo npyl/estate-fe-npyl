93b18ef09619a092b4beb944767439f4bdf71b54
npyl <npylarinos@digipath.gr>
2025-02-12 09:50:32 -0800

    Console error.

--- src/sections/Customer/Form/DemandDrawer/Content/getTab/RemoveButton.tsx
+++ src/sections/Customer/Form/DemandDrawer/Content/getTab/RemoveButton.tsx
@@ -1,11 +1,22 @@
-import { IconButton, SxProps, Theme } from "@mui/material";
+import { alpha, SxProps, Theme } from "@mui/material";
 import { MouseEvent, RefObject, useCallback } from "react";
 import { FC } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import { TabsRef } from "../types";
 
-const CloseButtonSx: SxProps<Theme> = {
+const CloseSx: SxProps<Theme> = {
     borderRadius: "100%",
+
+    color: "text.secondary",
+
+    "&:hover": {
+        color: "error.main",
+        bgcolor: ({
+            palette: {
+                error: { main },
+            },
+        }) => alpha(main, 0.1),
+    },
 };
 
 interface RemoveButtonProps {
@@ -15,7 +26,7 @@ interface RemoveButtonProps {
 
 const RemoveButton: FC<RemoveButtonProps> = ({ index, tabsRef }) => {
     const handleRemove = useCallback(
-        (e: MouseEvent<HTMLButtonElement>) => {
+        (e: MouseEvent<SVGSVGElement>) => {
             // IMPORTANT: do not trigger a tab click
             e.stopPropagation();
             tabsRef.current?.remove(index);
@@ -23,11 +34,7 @@ const RemoveButton: FC<RemoveButtonProps> = ({ index, tabsRef }) => {
         [index]
     );
 
-    return (
-        <IconButton size="small" onClick={handleRemove} sx={CloseButtonSx}>
-            <CloseIcon fontSize="small" />
-        </IconButton>
-    );
+    return <CloseIcon fontSize="small" onClick={handleRemove} sx={CloseSx} />;
 };
 
 export default RemoveButton;
