01c5f213070523bbf7a1f5b2916b52e84569616a
npyl <npylarinos@digipath.gr>
2025-02-13 01:19:55 -0800

    PP-461

--- src/sections/Properties/(FiltersBar)/Filters/useRangeGenerator.ts
+++ src/sections/Properties/(FiltersBar)/Filters/useRangeGenerator.ts
@@ -2,6 +2,11 @@ import { selectStates } from "@/slices/filters";
 import { useCallback } from "react";
 import { useSelector } from "react-redux";
 
+const HUNDRED_K = 100 * 1000;
+const HUNDRED = 100;
+const TEN_M = 10 * 1000 * 1000;
+const TEN_K = 10 * 1000;
+
 const useRangeGenerator = () => {
     const states = useSelector(selectStates);
 
@@ -9,18 +14,42 @@ const useRangeGenerator = () => {
         (type: "price" | "area") => {
             const numbers = [];
 
-            const HUNDRED_K = 100 * 1000;
-            const HUNDRED = 100;
-            const TEN_M = 10 * 1000 * 1000;
-            const TEN_K = 10 * 1000;
-
             if (type === "price") {
-                if (states.includes("SALE")) {
-                    for (let i = TEN_K; i <= TEN_M; i += HUNDRED_K - TEN_K) {
+                // RENT & RENTED or (RENT or RENTED) or nothing -> 2, 1, 0 lengths, respectively
+                const rentLength =
+                    states.includes("RENT") && states.includes("RENTED")
+                        ? 2
+                        : states.includes("RENT") || states.includes("RENTED")
+                        ? 1
+                        : 0;
+
+                const isRent = rentLength > 0;
+                const isSale = states.length > rentLength;
+
+                if (states.length === 0) {
+                    // nothing selected
+                    for (let i = HUNDRED; i <= TEN_K; i += HUNDRED) {
                         numbers.push(i);
                     }
-                } else {
+                } else if (isRent) {
+                    // RENT
                     for (let i = HUNDRED; i <= TEN_K; i += HUNDRED) {
+                        numbers.push(i);
+                    }
+
+                    // w/ SALE
+                    if (isSale) {
+                        for (
+                            let i = TEN_K;
+                            i <= TEN_M;
+                            i += HUNDRED_K - TEN_K
+                        ) {
+                            numbers.push(i);
+                        }
+                    }
+                } else {
+                    // SALE (exclusively)
+                    for (let i = TEN_K; i <= TEN_M; i += HUNDRED_K - TEN_K) {
                         numbers.push(i);
                     }
                 }
