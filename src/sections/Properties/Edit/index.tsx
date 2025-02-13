c21179ecca6b1778ea09011e6b275aeb4970a2cb
npyl <npylarinos@digipath.gr>
2025-02-13 19:10:25 +0200

    .

--- src/sections/Properties/Edit/index.tsx
+++ src/sections/Properties/Edit/index.tsx
@@ -2,13 +2,19 @@ import { useGetProperty } from "@/hooks/property";
 import Form from "./Form";
 import { useEditPropertyMutation } from "@/services/properties";
 import { useCallback } from "react";
+import { IPropertiesPOST } from "@/types/properties";
 
 const EditById = () => {
-    const { property } = useGetProperty();
+    const { property, propertyId: id } = useGetProperty();
 
     const [edit, { isSuccess }] = useEditPropertyMutation();
 
-    const handleSubmit = useCallback(() => {}, []);
+    const handleSubmit = useCallback(
+        async (body: IPropertiesPOST, generate: boolean) => {
+            const res = await edit({ id, body });
+        },
+        []
+    );
 
     return (
         <Form
