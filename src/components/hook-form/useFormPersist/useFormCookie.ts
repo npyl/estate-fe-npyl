7fa22537a0a2e6f26d893fe7f6f58dcef320a92d
npyl <pylarinosnick@gmail.com>
2025-02-27 14:15:02 +0200

    .

--- src/components/hook-form/useFormPersist/useFormCookie.ts
+++ src/components/hook-form/useFormPersist/useFormCookie.ts
@@ -2,7 +2,9 @@ import useCookie from "@/hooks/useCookie";
 import { EMPTY_FALLBACK } from "./constant";
 import { FieldValues } from "react-hook-form";
 
-const useFormCookie = <TFieldValues extends FieldValues>(cookieKey: string) => {
+const useFormCookie = <TFieldValues extends FieldValues>(
+    cookieKey: string | null
+) => {
     return useCookie<TFieldValues | typeof EMPTY_FALLBACK>(
         cookieKey,
         EMPTY_FALLBACK
