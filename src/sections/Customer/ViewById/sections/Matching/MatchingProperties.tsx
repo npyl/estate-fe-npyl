b76517018748bf6472e1c51baa0787d467205926
npyl <npylarinos@digipath.gr>
2025-03-06 15:08:55 +0200

    Experimental removal of customerSuggest-list

--- src/sections/Customer/ViewById/sections/Matching/MatchingProperties.tsx
+++ src/sections/Customer/ViewById/sections/Matching/MatchingProperties.tsx
@@ -1,11 +1,5 @@
-import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { useLoadApi } from "src/components/Map";
-import { ShapeData } from "src/components/Map/types";
-import { decodeShape, isPointInsideShapeData } from "src/components/Map/util";
-import useGetCustomer from "@/hooks/customer";
 import { useSuggestForCustomerQuery } from "src/services/properties";
-import { IProperties } from "src/types/properties";
 import Placeholder from "./Placeholder";
 import DataGrid from "@/components/DataGrid/Property";
 import Panel from "@/components/Panel";
@@ -14,70 +8,28 @@ import { Grid } from "@mui/material";
 import PropertyCard from "@/components/Cards/PropertyCard";
 import { usePagination } from "@/components/Pagination";
 import Pagination from "@/components/Pagination/client";
-
-const filterPropertiesInShape = (
-    properties: IProperties[],
-    shapeData: ShapeData
-): IProperties[] =>
-    properties.filter(
-        (p) =>
-            !p.location?.lat ||
-            !p.location?.lng ||
-            isPointInsideShapeData(p.location?.lat, p.location?.lng, shapeData)
-    );
+import { useRouter } from "next/router";
+import { toNumberSafe } from "@/utils/toNumber";
 
 const pageSize = 5;
 
 const MatchingPropertiesSection = () => {
     const { t } = useTranslation();
 
-    const { isLoaded } = useLoadApi(); // google maps api
-
-    const { customer, customerId } = useGetCustomer();
-
     const pagination = usePagination();
 
-    const { data, isLoading } = useSuggestForCustomerQuery({
-        customerId: +customerId!,
-    });
+    const router = useRouter();
+    const { customerId } = router.query;
+    const iCustomerId = toNumberSafe(customerId);
 
-    const demands = useMemo(() => customer?.demands || [], [customer?.demands]);
+    const { data, isLoading } = useSuggestForCustomerQuery(
+        {
+            customerId: iCustomerId,
+        },
+        { skip: iCustomerId === -1 }
+    );
 
-    const properties = useMemo(() => {
-        if (!isLoaded) return [];
-        if (!data) return [];
-
-        const haveNoShapes = demands?.every((demand) => {
-            const shapes = demand?.shapes;
-            if (!shapes) return true; // every
-            return shapes.every((shape) => !shape);
-        });
-
-        // If we have no shapes in our demands; return just the content from the backend
-        if (haveNoShapes) return data;
-
-        // Otherwise, for every demand
-        const res = demands
-            ?.map((demand) => {
-                // Get all shapes
-                const shapes = demand?.shapes;
-                return shapes
-                    ?.map((shape) => {
-                        // For every shape
-                        const shapeData = decodeShape(shape);
-
-                        // Return filtered properties
-                        return shapeData
-                            ? filterPropertiesInShape(data, shapeData)
-                            : [];
-                    })
-                    .flat();
-            })
-            .flat();
-
-        // Keep only the unique entries
-        return [...new Set(res)];
-    }, [isLoaded, data, demands]);
+    const properties = data?.content || [];
 
     const belowLg = useResponsive("down", "lg");
 
