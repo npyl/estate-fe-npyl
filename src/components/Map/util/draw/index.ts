f73576b7df97f0966003e46d4bd0b7c7be847819
npyl <pylarinosnick@gmail.com>
2025-03-11 11:44:01 +0200

    Normalise a backend-received shape

--- src/components/Map/util/draw/index.ts
+++ src/components/Map/util/draw/index.ts
@@ -22,17 +22,32 @@ const getShapeType = (s: TShape): TShapeType | null => {
     return null;
 };
 
+/**
+ * This method is REALLY important! Backend has asked us to treat a 0.0 value as null; This is important for circles where y is null for radius.
+ * @param s a shape as received from Backend
+ */
+const normaliseShape = (s: TShape) => {
+    if (s.length === 2 && s[1].y === 0.0)
+        return [
+            { x: s[0].x, y: s[0].y },
+            { x: s[1].x, y: null },
+        ];
+    else return s;
+};
+
 // --------------------------------------------------------------------------------
 
 /**
  * Draws a (visible) shape to a google map & returns the object that was generated
  */
 const drawShape = (
-    shape: TShape,
+    s: TShape,
     map: google.maps.Map,
     changeable: boolean
 ): DrawShape | null => {
-    if (!shape || shape.length === 0) return null;
+    if (!s || s.length === 0) return null;
+
+    const shape = normaliseShape(s);
 
     const type = getShapeType(shape);
 
