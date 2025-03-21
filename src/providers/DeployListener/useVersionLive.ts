9097fa89a2bbdee4f76dff882933bc2a5ffb4070
npyl <npylarinos@digipath.gr>
2025-03-21 16:10:07 +0200

    .

--- src/providers/DeployListener/useVersionLive.ts
+++ src/providers/DeployListener/useVersionLive.ts
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { TVersionCb } from "./types";
-import debugLog from "@/_private/debugLog";
 import { IVersionRes } from "@/types/server";
 
 const clientId = process.env.NEXT_PUBLIC_CLIENT_ID || "";
@@ -9,9 +8,6 @@ const sseUrl = `/api/server/sse/${clientId}`;
 const useVersionLive = (handler: TVersionCb) => {
     useEffect(() => {
         const src = new EventSource(sseUrl, { withCredentials: false });
-
-        src.onopen = debugLog;
-        src.onerror = debugLog;
 
         src.onmessage = (ev: MessageEvent) => {
             const data = JSON.parseSafe<IVersionRes>(ev.data);
