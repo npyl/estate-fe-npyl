bc72f4e8e854e15ae6178cf5b6ec29782351c1c8
npyl <npylarinos@digipath.gr>
2025-03-26 10:59:52 +0200

    Move out.

--- src/components/dashboard/dashboard-subbar/index.tsx
+++ src/components/dashboard/dashboard-subbar/index.tsx
@@ -1,11 +1,31 @@
-import { SubbarRef } from "@/contexts/tabs";
+import { useRouter } from "next/router";
+import SubbarItems from "./Items";
 import { forwardRef } from "react";
-import DesktopBar from "./Desktop";
+import { SubbarRef } from "@/contexts/tabs";
 
-const Subbar = forwardRef<SubbarRef, object>(({}, ref) => (
-    <DesktopBar ref={ref} />
-));
+const DesktopBar = forwardRef<SubbarRef, object>(({}, ref) => {
+    const router = useRouter();
 
-Subbar.displayName = "Subbar";
+    //use these two specific paths so the subbar is sticky in edit and create property form
+    const isStickyPath =
+        router.pathname.startsWith("/property/edit") ||
+        router.pathname === "/property/create";
 
-export default Subbar;
+    return (
+        <SubbarItems
+            ref={ref}
+            overflow="auto"
+            width={1}
+            sx={{
+                display: { xs: "none", lg: "flex" },
+                position: isStickyPath ? "sticky" : "relative",
+                top: isStickyPath ? 64 : 0,
+                zIndex: 100,
+            }}
+        />
+    );
+});
+
+DesktopBar.displayName = "DesktopBar";
+
+export default DesktopBar;
