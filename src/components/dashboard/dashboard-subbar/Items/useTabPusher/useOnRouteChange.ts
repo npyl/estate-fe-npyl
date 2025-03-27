2fde806c77a170b568a22e580d5c546e32970bfa
npyl <pylarinosnick@gmail.com>
2025-03-27 15:53:33 +0200

    .

--- src/components/dashboard/dashboard-subbar/Items/useTabPusher/useOnRouteChange.ts
+++ src/components/dashboard/dashboard-subbar/Items/useTabPusher/useOnRouteChange.ts
@@ -1,11 +1,11 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 // Handle in-app navigation
 const useOnRouteChange = (cb: (url: string) => void) => {
     const path = useRouter().asPath;
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         cb(path);
     }, [cb, path]);
 };
