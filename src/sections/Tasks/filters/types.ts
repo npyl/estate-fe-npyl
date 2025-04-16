83d2dd3e4844291b634269b486213b9ac8b51ddb
npyl <npylarinos@digipath.gr>
2025-04-16 15:48:53 +0300

    PP-611

---
+++ src/sections/Tasks/filters/types.ts
@@ -0,0 +1,9 @@
+import { TSorting } from "@/sections/Filters/SortBy/types";
+import { BoardFiltersReq } from "@/types/tasks";
+
+type TFilters = Omit<BoardFiltersReq, "search"> & {
+    search: string; // INFO: required to avoid undefined as initial value
+    sorting?: TSorting;
+};
+
+export type { TFilters };
---
+++ src/sections/Tasks/filters/useFilterState.ts
@@ -0,0 +1,47 @@
+import useLocalStorage from "@/hooks/useLocalStorage";
+import { initialState } from "./constants";
+import { TFilters } from "./types";
+import { useAuth } from "@/hooks/use-auth";
+import { useCallback, useMemo } from "react";
+import useCallbackSetter from "@/hooks/useCallbackSetter";
+
+type TFiltersState = Record<number, TFilters>;
+
+const storageKey = "task-filters";
+
+const getFiltersSafe = (ts: TFiltersState, userId: number): TFilters => {
+    try {
+        const filters = ts?.[userId];
+
+        if (!filters) return initialState;
+
+        return { ...filters, labels: filters?.labels || [] };
+    } catch (ex) {
+        return initialState;
+    }
+};
+
+const useFilterState = () => {
+    const { user } = useAuth();
+    const [_state, __setState] = useLocalStorage<TFiltersState>(storageKey, {});
+
+    const state = useMemo(
+        () => getFiltersSafe(_state, user?.id ?? -1),
+        [_state, user?.id]
+    );
+
+    const _setState = useCallback(
+        (f: TFilters) =>
+            __setState((old) => ({
+                ...old,
+                [user?.id!]: f,
+            })),
+        [user?.id]
+    );
+
+    const setState = useCallbackSetter<TFilters>(state, _setState);
+
+    return [state, setState] as const;
+};
+
+export default useFilterState;
