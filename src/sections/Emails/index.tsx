baa5486d6a66fb8acfdc3565fc315b211bf9f779
npyl <npylarinos@digipath.gr>
2025-05-07 14:21:21 +0300

    Property by id.

---
+++ src/sections/Emails/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+interface Props {
+    customerId?: number;
+    propertyId?: number;
+}
+
+const ViewAll: FC<Props> = ({ customerId, propertyId }) => {
+    return null;
+};
+
+export default ViewAll;
--- src/sections/Properties/ViewById/(tabs)/Emails/index.tsx
+++ src/sections/Properties/ViewById/(tabs)/Emails/index.tsx
@@ -1,2 +1,14 @@
-const Emails = () => null;
+import ViewAll from "@/sections/Emails";
+import { toNumberSafe } from "@/utils/toNumber";
+import { useRouter } from "next/router";
+
+const Emails = () => {
+    const router = useRouter();
+    const { propertyId } = router.query;
+    const iPropertyId = toNumberSafe(propertyId);
+    if (iPropertyId === -1) return null;
+
+    return <ViewAll propertyId={iPropertyId} />;
+};
+
 export default Emails;
