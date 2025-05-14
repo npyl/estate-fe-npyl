1a1f2f81a1e7b5732814f9bf3e1e520cb8ae66de
npyl <npylarinos@digipath.gr>
2025-05-14 12:12:27 +0300

    Fix not selecting from.

--- src/sections/_Pickers/UserPicker/MoreAvatars.tsx
+++ src/sections/_Pickers/UserPicker/MoreAvatars.tsx
@@ -1,23 +1,25 @@
 import Popover from "@mui/material/Popover";
 import { FC } from "react";
 import { MoreAvatarsProps } from "@/components/Avatar/Group/types";
-import { useFiltersContext } from "@/sections/Calendar/Filters/context";
 import AvatarSelectGroup from "@/components/Avatar/SelectGroup";
 
-const MoreAvatars: FC<MoreAvatarsProps> = ({ users, anchorEl, onClose }) => {
-    const { calendarId, setCalendarId } = useFiltersContext();
-
-    return (
-        <Popover open anchorEl={anchorEl} onClose={onClose}>
-            <AvatarSelectGroup
-                users={users}
-                max={users.length}
-                // ...
-                value={calendarId}
-                onChange={setCalendarId}
-            />
-        </Popover>
-    );
-};
+const MoreAvatars: FC<MoreAvatarsProps> = ({
+    value,
+    onChange,
+    // ...
+    users,
+    anchorEl,
+    onClose,
+}) => (
+    <Popover open anchorEl={anchorEl} onClose={onClose}>
+        <AvatarSelectGroup
+            users={users}
+            max={users.length}
+            // ...
+            value={value}
+            onChange={(v) => onChange?.(v)}
+        />
+    </Popover>
+);
 
 export default MoreAvatars;
