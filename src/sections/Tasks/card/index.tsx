8d8d931384d74262ba24976eb1067b111bddd40a
ntinos_kost <ntinos_kost@digipath.gr>
2025-05-15 15:34:04 +0300

    Show label in task card

--- src/sections/Tasks/card/index.tsx
+++ src/sections/Tasks/card/index.tsx
@@ -7,6 +7,7 @@ import Content from "./Content";
 import { Paper, PaperProps } from "@mui/material";
 import { forwardRef } from "react";
 import Link from "@/components/Link";
+import TaskLabel from "./TaskLabel";
 
 // ----------------------------------------------------------------------
 
@@ -16,8 +17,8 @@ export type TaskCardProps = PaperProps & {
 
 const TaskCard = forwardRef<HTMLDivElement, TaskCardProps>(
     ({ card, sx, ...props }, ref) => {
-        const { name, completed, priority, assignees, uniqueCode } = card || {};
-
+        const { name, completed, priority, assignees, uniqueCode, labels } =
+            card || {};
         return (
             <Paper
                 ref={ref}
@@ -35,6 +36,10 @@ const TaskCard = forwardRef<HTMLDivElement, TaskCardProps>(
                 />
 
                 <Content name={name} />
+                {/* The box ensures the same size of all cards */}
+                <Box minHeight={22}>
+                    <TaskLabel labels={labels} />
+                </Box>
 
                 <Box flexGrow={2} />
 
