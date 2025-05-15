4f171f6f83bce42069bfdaf7b001275f79f8e8b8
npyl <pylarinosnick@gmail.com>
2025-05-15 11:01:55 +0300

    Tasks: title should be ellipsis with tooltip (only on overflow)

--- src/sections/Tasks/card/Content.tsx
+++ src/sections/Tasks/card/Content.tsx
@@ -1,25 +1,67 @@
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { FC } from "react";
-import PriorityLabel from "./PriorityLabel";
+import Tooltip from "@mui/material/Tooltip";
+import {
+    FC,
+    useRef,
+    useState,
+    useLayoutEffect,
+    useCallback,
+    RefObject,
+} from "react";
 
-// ------------------------------------------------------------------
+const useIsEllipsis = (elementRef: RefObject<HTMLElement>) => {
+    const [isEllipsis, setIsOverflown] = useState(false);
+
+    const checkOverflow = useCallback(() => {
+        const element = elementRef.current;
+        if (!element) return;
+        setIsOverflown(element.scrollWidth > element.clientWidth);
+    }, []);
+
+    useLayoutEffect(() => {
+        // Initial check
+        checkOverflow();
+
+        // Re-check on window resize
+        window.addEventListener("resize", checkOverflow);
+
+        return () => {
+            window.removeEventListener("resize", checkOverflow);
+        };
+    }, []);
+
+    return isEllipsis;
+};
 
 const EllipsisSx = {
     overflow: "hidden",
     textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
 };
 
 interface ContentProps {
     name: string;
 }
 
-const Content: FC<ContentProps> = ({ name }) => (
-    <Stack direction="row" spacing={1}>
-        <Typography sx={EllipsisSx} variant="body2">
+const Content: FC<ContentProps> = ({ name }) => {
+    const textRef = useRef<HTMLElement>(null);
+
+    const isEllipsis = useIsEllipsis(textRef);
+
+    const content = (
+        <Typography ref={textRef} sx={EllipsisSx} variant="body2">
             {name}
         </Typography>
-    </Stack>
-);
+    );
+
+    if (isEllipsis)
+        return (
+            <Tooltip title={name} placement="top">
+                {content}
+            </Tooltip>
+        );
+
+    return content;
+};
 
 export default Content;
