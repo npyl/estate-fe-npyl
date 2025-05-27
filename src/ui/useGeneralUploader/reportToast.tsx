6b3bf83797d78cb3f4cce78e84696afd4b4aba71
npyl <npylarinos@digipath.gr>
2025-05-27 21:15:11 +0300

    Implement (?) the reportToast

--- src/ui/useGeneralUploader/reportToast.tsx
+++ src/ui/useGeneralUploader/reportToast.tsx
@@ -4,23 +4,32 @@ import { FC } from "react";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { useTranslation } from "react-i18next";
+import { SpaceBetween } from "@/components/styled";
+import CloseButton from "@/components/Toaster/Message/CloseButton";
 
-const getFailed = (name: string) => (
-    <Typography key={name} fontWeight="600">
-        {name}
-    </Typography>
-);
+const getFailed = (name: string) => <Typography key={name}>{name}</Typography>;
 
 interface MessageProps {
     r: IUploadReport;
 }
 
-const Message: FC<MessageProps> = ({ r }) => {
+const Message: FC<MessageProps> = ({ r, ...other }) => {
     const { t } = useTranslation();
+
+    // INFO: onDismiss is automatically passed to every message that is react component (from @/components/Toast)
+    const onDismiss =
+        "onDismiss" in other ? (other.onDismiss as VoidFunction) : undefined;
 
     return (
         <Stack spacing={1}>
-            <Typography>{t("UPLOAD_FAILED_FILES")}</Typography>
+            <SpaceBetween>
+                <Typography fontWeight="bold" fontSize="16px">
+                    {t("UPLOAD_FAILED_FILES")}
+                </Typography>
+
+                {onDismiss ? <CloseButton /> : null}
+            </SpaceBetween>
+
             <Stack>{r.uploadFails.map(getFailed)}</Stack>
         </Stack>
     );
