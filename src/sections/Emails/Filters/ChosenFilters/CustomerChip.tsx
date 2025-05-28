8855e0210322877af96d3db766363b7fe182f57b
npyl <npylarinos@digipath.gr>
2025-05-28 17:09:58 +0300

    Return CustomerChip.

--- src/sections/Emails/Filters/ChosenFilters/CustomerChip.tsx
+++ src/sections/Emails/Filters/ChosenFilters/CustomerChip.tsx
@@ -3,6 +3,7 @@ import ChipLabel from "@/sections/Filters/ChipLabel";
 import { useTranslation } from "react-i18next";
 import { FC } from "react";
 import { useGetCustomerByIdQuery } from "@/services/customers";
+import { useFiltersContext } from "../Context";
 
 interface Props {
     id: number;
@@ -11,10 +12,12 @@ interface Props {
 const CustomerChip: FC<Props> = ({ id }) => {
     const { t } = useTranslation();
     const { data } = useGetCustomerByIdQuery(id);
+    const { box } = useFiltersContext();
 
+    const title = box === "INBOX" ? t("From") : t("To");
     const fullname = `${data?.firstName || ""} ${data?.lastName || ""}`;
 
-    return <Chip label={<ChipLabel title={t("To")} value={fullname} />} />;
+    return <Chip label={<ChipLabel title={title} value={fullname} />} />;
 };
 
 export default CustomerChip;
