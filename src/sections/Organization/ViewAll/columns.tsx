7f2e83392788567ad9112d5b6c7f41ea237780cf
npyl <pylarinosnick@gmail.com>
2025-05-30 13:45:26 +0300

    Add avatar to columns.

--- src/sections/Organization/ViewAll/columns.tsx
+++ src/sections/Organization/ViewAll/columns.tsx
@@ -1,7 +1,21 @@
 import { GridColDef } from "@mui/x-data-grid";
 import { TranslationType } from "@/types/translation";
+import Avatar from "@/components/Avatar";
+import Stack from "@mui/material/Stack";
 
 const getColumns = (t: TranslationType): GridColDef[] => [
+    {
+        field: "avatar",
+        headerName: "",
+        headerAlign: "left",
+        align: "left",
+        renderCell: (p) => (
+            <Stack height={1} alignItems="center" justifyContent="center">
+                <Avatar src={p.row?.avatar || ""} />
+            </Stack>
+        ),
+    },
+
     {
         flex: 1.2,
         field: "name",
