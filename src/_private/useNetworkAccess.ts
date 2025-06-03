cae9346c2b2942ba7165fdda3c1ab633a5156a42
npyl <npylarinos@digipath.gr>
2025-06-03 10:42:49 +0300

    Fix network status checking? (with better pinging?)

--- src/_private/useNetworkAccess.ts
+++ src/_private/useNetworkAccess.ts
@@ -1,21 +1,33 @@
 import { useCallback, useLayoutEffect, useRef } from "react";
 
 const INTERVAL = 30 * 1000; // 30sec (in ms)
-const INSTANT = 300;
+const INSTANT = 1000;
 
-const checkConnectivity = async () => {
+const ping = async () => {
     try {
-        await fetch("https://www.google.com", {
+        const res = await fetch("https://www.google.com", {
             method: "HEAD",
             mode: "no-cors",
             cache: "no-cache",
             signal: AbortSignal.timeout(INSTANT),
         });
 
-        return true;
-    } catch {
+        return res.type === "opaque"; // INFO: this is an opaque response; check if we got any
+    } catch (ex) {
         return false;
     }
+};
+
+const checkConnectivity = async () => {
+    const res = await ping();
+
+    // INFO: if a ping fails, run 3 pings to be sure
+    if (!res) {
+        const all = await Promise.all([ping(), ping(), ping()]);
+        return all.some(Boolean);
+    }
+
+    return true;
 };
 
 interface UseNetworkAccessOptions {
