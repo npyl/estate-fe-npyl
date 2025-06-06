a3e6735d9a420d67d7131ee76f75e612f66a34cd
npyl <npylarinos@digipath.gr>
2025-06-06 19:57:46 +0300

    Add timeout support

--- tests/_util/expectValue.ts
+++ tests/_util/expectValue.ts
@@ -3,10 +3,11 @@ import { expect, MountResult } from "@playwright/experimental-ct-react";
 const expectValue = async (
     component: MountResult,
     ID: string,
-    VALUE: string
+    VALUE: string,
+    timeout?: number
 ) => {
     const valueLocator = component.getByTestId(ID);
-    await expect(valueLocator).toHaveText(VALUE);
+    await expect(valueLocator).toHaveText(VALUE, { timeout });
 };
 
 export default expectValue;
