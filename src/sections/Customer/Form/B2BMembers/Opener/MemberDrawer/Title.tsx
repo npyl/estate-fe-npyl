2e0997c938c12bd421f1aeea31231b1824326247
npyl <npylarinos@digipath.gr>
2025-06-06 11:26:22 +0300

    Better title.

--- src/sections/Customer/Form/B2BMembers/Opener/MemberDrawer/Title.tsx
+++ src/sections/Customer/Form/B2BMembers/Opener/MemberDrawer/Title.tsx
@@ -1,11 +1,24 @@
+import { B2BMemberReq } from "@/types/customer";
 import Typography from "@mui/material/Typography";
+import { FC } from "react";
 import { useTranslation } from "react-i18next";
 
-const Title = () => {
+interface TitleProps {
+    member?: B2BMemberReq;
+}
+
+const Title: FC<TitleProps> = ({ member }) => {
     const { t } = useTranslation();
+
+    const { firstName, lastName } = member || {};
+    const fullname = `${firstName || ""} ${lastName || ""}`;
+
+    const title = !Boolean(member) ? t("Add Member") : fullname;
+
     return (
         <Typography px={0.5} variant="h6">
-            {t("Member")}
+            {" "}
+            {title}
         </Typography>
     );
 };
