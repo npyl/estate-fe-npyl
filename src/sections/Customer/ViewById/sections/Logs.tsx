d39d322be0b943c97f2e2c9a6825fa9ec7fb9720
npyl <npylarinos@digipath.gr>
2025-06-11 13:36:39 +0300

    Convert to use @/sections/Logs/ViewAll instead of keeping copies of the same thing only slightly different.

--- src/sections/Customer/ViewById/sections/Logs.tsx
+++ src/sections/Customer/ViewById/sections/Logs.tsx
@@ -1,52 +1,14 @@
-import { useState } from "react";
-import { useCustomerHistoryPaginatedQuery } from "src/services/logs";
-import { Box, Pagination, Stack } from "@mui/material";
-import { NextPage } from "next";
-import LogCard from "@/components/Cards/LogCard";
+import ViewAll from "@/sections/Logs/ViewAll";
+import { toNumberSafe } from "@/utils/toNumber";
 import { useRouter } from "next/router";
 
-const CustomerLogs: NextPage = () => {
-    const [page, setPage] = useState(0);
-    const [pageSize, setPageSize] = useState(10);
+const Logs = () => {
     const router = useRouter();
     const { customerId } = router.query;
-    const { data } = useCustomerHistoryPaginatedQuery({
-        id: +customerId!,
-        page,
-        pageSize,
-    });
+    const iCustomerId = toNumberSafe(customerId);
+    if (iCustomerId === -1) return null;
 
-    const handlePageChange = (_: any, value: number) => {
-        setPage(value - 1); // Adjust page number for zero-based numbering API
-    };
-
-    // Main content to render
-    const content = data?.content.map((log) => (
-        <LogCard key={log.createdAt} log={log} />
-    ));
-
-    return (
-        <Box>
-            <Stack spacing={1}>{content}</Stack>
-            {data && ( // Only display pagination when data is loaded
-                <Box
-                    display="flex" // Establishes a flex container
-                    justifyContent="center" // Centers items on the main axis
-                    alignItems="center" // Centers items on the cross axis
-                    my={3} // Adds spacing around the Box, change as needed
-                >
-                    <Pagination
-                        count={data.totalPages}
-                        page={page + 1} // Adjust for 1-based numbering of Material-UI Pagination
-                        onChange={handlePageChange}
-                        color="primary"
-                        showFirstButton
-                        showLastButton
-                    />
-                </Box>
-            )}
-        </Box>
-    );
+    return <ViewAll propertyId={iCustomerId} />;
 };
 
-export default CustomerLogs;
+export default Logs;
--- src/sections/Properties/ViewById/(tabs)/Logs.tsx
+++ src/sections/Properties/ViewById/(tabs)/Logs.tsx
@@ -1,56 +1,14 @@
-import { useState } from "react";
-import { usePropertyHistoryPaginatedQuery } from "src/services/logs";
-import { Box, Pagination, Stack } from "@mui/material";
-import { NextPage } from "next";
-import LogCard from "@/components/Cards/LogCard";
+import ViewAll from "@/sections/Logs/ViewAll";
+import { toNumberSafe } from "@/utils/toNumber";
 import { useRouter } from "next/router";
 
-const PropertyLogs: NextPage = () => {
-    const [page, setPage] = useState(0);
-    const [pageSize, setPageSize] = useState(10);
-
+const Logs = () => {
     const router = useRouter();
     const { propertyId } = router.query;
-    const { data } = usePropertyHistoryPaginatedQuery({
-        id: +propertyId!,
-        page,
-        pageSize,
-    });
+    const iPropertyId = toNumberSafe(propertyId);
+    if (iPropertyId === -1) return null;
 
-    const handlePageChange = (
-        event: React.ChangeEvent<unknown>,
-        value: number
-    ) => {
-        setPage(value - 1); // Adjust page number for zero-based numbering API
-    };
-
-    // Main content to render
-    const content = data?.content.map((log) => (
-        <LogCard key={log.createdAt} log={log} />
-    ));
-
-    return (
-        <Box>
-            <Stack spacing={1}>{content}</Stack>
-            {data && ( // Only display pagination when data is loaded
-                <Box
-                    display="flex" // Establishes a flex container
-                    justifyContent="center" // Centers items on the main axis
-                    alignItems="center" // Centers items on the cross axis
-                    my={3} // Adds spacing around the Box, change as needed
-                >
-                    <Pagination
-                        count={data.totalPages}
-                        page={page + 1} // Adjust for 1-based numbering of Material-UI Pagination
-                        onChange={handlePageChange}
-                        color="primary"
-                        showFirstButton
-                        showLastButton
-                    />
-                </Box>
-            )}
-        </Box>
-    );
+    return <ViewAll propertyId={iPropertyId} />;
 };
 
-export default PropertyLogs;
+export default Logs;
