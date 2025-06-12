0ca4e8758a7b032e5b9494bdca961385c6b4f26e
npyl <pylarinosnick@gmail.com>
2025-06-12 11:37:16 +0300

    .

--- src/sections/Customer/ViewById/sections/Information/index.tsx
+++ src/sections/Customer/ViewById/sections/Information/index.tsx
@@ -1,12 +1,19 @@
 import { Stack } from "@mui/material";
-import B2BMembers from "./B2BMembers";
-import Information from "./Information";
+import Information from "./View";
+import useGetCustomer from "@/hooks/customer";
+import dynamic from "next/dynamic";
+const B2BMembers = dynamic(() => import("./B2BMembers"));
 
-const InformationSection = () => (
-    <Stack spacing={1}>
-        <Information />
-        <B2BMembers />
-    </Stack>
-);
+const InformationSection = () => {
+    const { customer } = useGetCustomer();
+    const { members } = customer || {};
+    const hasMembers = members?.length && members?.length > 0;
+    return (
+        <Stack spacing={1}>
+            <Information />
+            {hasMembers ? <B2BMembers /> : null}
+        </Stack>
+    );
+};
 
 export default InformationSection;
