df23f2e3d4026c31ef3d8cf8e58a2f992eac91b0
npyl <npylarinos@digipath.gr>
2025-07-01 10:41:20 +0300

    .

--- src/sections/Blog/ViewAll/Filters/Bar/index.tsx
+++ src/sections/Blog/ViewAll/Filters/Bar/index.tsx
@@ -2,14 +2,16 @@ import PPFiltersBar from "@/components/Filters/FiltersBar";
 import Search from "./Search";
 import Sites from "./Sites";
 import User from "./User";
+import State from "./State";
 
 const FiltersBar = () => (
     <PPFiltersBar
         filters={
             <>
                 <Search />
-                <Sites />
                 <User />
+                <Sites />
+                <State />
             </>
         }
         bottomContent={undefined}
