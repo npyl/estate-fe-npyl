d3c3fe0de2caf3005a7a66371919fa0a7fdf0291
npyl <npylarinos@digipath.gr>
2025-07-24 11:56:14 +0300

    Standardize!

--- tests/useGeneralUpload/_util/getNetworkControl.ts
+++ tests/useGeneralUpload/_util/getNetworkControl.ts
@@ -1,11 +1,10 @@
 import { Page } from "@playwright/test";
-
-// Use the EXACT URL from your hook's fetch call
-const PING_URL = "https://www.google.com";
+import { PING_URL } from "../../../src/_private/useNetworkAccess";
 
 const getNetworkControl = async (page: Page) => {
     let isOnline = true;
 
+    // IMPORTANT: we need to catch EXACTLY the same url
     await page.route(PING_URL, async (route) => {
         if (isOnline) {
             await route.fulfill({ status: 200 });
