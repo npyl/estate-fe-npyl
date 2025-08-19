a880f5cf3a094576f1bd022cf9b4f86d2ce0d3d2
npyl <pylarinosnick@gmail.com>
2025-08-19 12:12:18 +0300

    Double down on a test structure.

--- tests/components/hook-form/Select/index.spec.tsx
+++ tests/components/hook-form/Select/index.spec.tsx
@@ -9,6 +9,20 @@ import { NOT_SELECTED_VALUE } from "../../../../src/constants/select";
 import { OPTIONS } from "./constants";
 import { Page } from "playwright-core";
 
+// -------------------------------------------------------------
+// Set          - click on an option
+// Set other    - click on a different option
+// Re-set       - click on a previous option
+// Clear        - click on "not-selected" option
+// -------------------------------------------------------------
+// Testing Set, Set other, Re-set & Clear for all these cases:
+// 1. not multiple, not enum (a.k.a. single selection)
+// 2. multiple, not enum (a.k.a. multiple)
+// 3. not multiple, yes enum
+// 4. yes multiple, yes enum
+
+// ----------------------------------------------------------------------------------------
+
 const clickAndExpect = async (
     component: MountResult,
     page: Page,
@@ -25,21 +39,22 @@ const clickAndExpect = async (
     await expect(component.getByTestId(VALUE_ID)).toHaveText(value);
 };
 
-// Testing set & clear for all these cases:
-// 1. not multiple, not enum (a.k.a. single selection)
-// 2. multiple, not enum (a.k.a. multiple)
-// 3. not multiple, yes enum
-// 4. yes multiple, yes enum
+// ----------------------------------------------------------------------------------------
 
-test("Set & Clear (Single)", async ({ mount, page }) => {
+test("Set, Set other & Clear (Single)", async ({ mount, page }) => {
     const component = await mount(<Tester />);
 
-    // Click & Expect Option
-    const OPTION = OPTIONS[1];
-    const OPTION_TEST_ID = getOptionTestId(OPTION.key);
-    await clickAndExpect(component, page, [OPTION_TEST_ID], OPTION.key);
+    // Set
+    const OPTION0 = OPTIONS[0];
+    const OPTION0_TEST_ID = getOptionTestId(OPTION0.key);
+    await clickAndExpect(component, page, [OPTION0_TEST_ID], OPTION0.key);
 
-    // Clear & Expect
+    // Set other
+    const OPTION1 = OPTIONS[1];
+    const OPTION1_TEST_ID = getOptionTestId(OPTION1.key);
+    await clickAndExpect(component, page, [OPTION1_TEST_ID], OPTION1.key);
+
+    // Clear
     await clickAndExpect(
         component,
         page,
@@ -48,22 +63,77 @@ test("Set & Clear (Single)", async ({ mount, page }) => {
     );
 });
 
-test("Set & Clear (Multiple)", async ({ mount, page }) => {
+test("Set, Set other, Re-set (Multiple)", async ({ mount, page }) => {
     const component = await mount(<Tester multiple />);
 
-    // Click & Expect Option
+    // Set
     const OPTION0 = OPTIONS[0];
-    const OPTION1 = OPTIONS[1];
     const OPTION0_TEST_ID = getOptionTestId(OPTION0.key);
+    const EXPECTED0 = JSON.stringify([OPTION0.key]);
+    await clickAndExpect(component, page, [OPTION0_TEST_ID], EXPECTED0);
+
+    // Set other
+    const OPTION1 = OPTIONS[1];
     const OPTION1_TEST_ID = getOptionTestId(OPTION1.key);
+    const EXPECTED1 = JSON.stringify([OPTION0.key, OPTION1.key]);
+    await clickAndExpect(component, page, [OPTION1_TEST_ID], EXPECTED1);
+
+    // Re-set (re-click OPTION1)
+    await clickAndExpect(component, page, [OPTION1_TEST_ID], EXPECTED0);
+
+    // Clear
     await clickAndExpect(
         component,
         page,
-        [OPTION0_TEST_ID, OPTION1_TEST_ID],
-        JSON.stringify([OPTION0.key, OPTION1.key])
+        [NOT_SELECTED_TESTID],
+        JSON.stringify(null)
     );
+});
 
-    // Clear & Expect
+test("Set, Set other (Single & Enum)", async ({ mount, page }) => {
+    const component = await mount(<Tester isEnum />);
+
+    // Set
+    const OPTION0 = OPTIONS[0];
+    const OPTION0_TEST_ID = getOptionTestId(OPTION0.key);
+    await clickAndExpect(component, page, [OPTION0_TEST_ID], OPTION0.key);
+
+    // Set other
+    const OPTION1 = OPTIONS[1];
+    const OPTION1_TEST_ID = getOptionTestId(OPTION1.key);
+    await clickAndExpect(component, page, [OPTION1_TEST_ID], OPTION1.key);
+
+    // Clear
+    await clickAndExpect(
+        component,
+        page,
+        [NOT_SELECTED_TESTID],
+        JSON.stringify(null)
+    );
+});
+
+test("Set, Set other, Re-set & Clear (Multiple & Enum)", async ({
+    mount,
+    page,
+}) => {
+    const component = await mount(<Tester multiple isEnum />);
+
+    // Set
+    const OPTION0 = OPTIONS[0];
+    const OPTION0_TEST_ID = getOptionTestId(OPTION0.key);
+    const EXPECTED0 = JSON.stringify([OPTION0.key]);
+    await clickAndExpect(component, page, [OPTION0_TEST_ID], EXPECTED0);
+
+    // Set other
+    const OPTION1 = OPTIONS[1];
+    const OPTION1_TEST_ID = getOptionTestId(OPTION1.key);
+    const EXPECTED1 = JSON.stringify([OPTION0.key, OPTION1.key]);
+    await clickAndExpect(component, page, [OPTION1_TEST_ID], EXPECTED1);
+
+    // Re-set (re-click OPTION1)
+    await clickAndExpect(component, page, [OPTION1_TEST_ID], EXPECTED0);
+
+    // Clear
     await clickAndExpect(
         component,
         page,
@@ -71,11 +141,3 @@ test("Set & Clear (Multiple)", async ({ mount, page }) => {
         JSON.stringify([])
     );
 });
-
-// test("Set & Clear (Single & Enum)", async ({ mount }) => {
-//     const component = await mount(<Tester isEnum />);
-// });
-
-// test("Set & Clear (Single & Multiple)", async ({ mount }) => {
-//     const component = await mount(<Tester multiple isEnum />);
-// });
