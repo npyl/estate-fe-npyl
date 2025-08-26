de9e8d4029ced561bf1ba4a6fbbbac6ad545f91a
npyl <pylarinosnick@gmail.com>
2025-08-26 19:51:33 +0300

    LCov for Sonar

--- jest.config.ts
+++ jest.config.ts
@@ -9,27 +9,16 @@ const config: Config = {
     // Automatically clear mock calls, instances, contexts and results before every test
     clearMocks: true,
 
-    // Indicates whether the coverage information should be collected while executing the test
-    collectCoverage: true,
-
-    // The directory where Jest should output its coverage files
-    coverageDirectory: "coverage",
-
     // An array of regexp pattern strings used to skip coverage collection
     // coveragePathIgnorePatterns: [
     //   "\\\\node_modules\\\\"
     // ],
 
-    // Indicates which provider should be used to instrument code for coverage
+    // Coverage
+    collectCoverage: true,
+    coverageDirectory: "coverage",
     coverageProvider: "v8",
-
-    // A list of reporter names that Jest uses when writing coverage reports
-    // coverageReporters: [
-    //   "json",
-    //   "text",
-    //   "lcov",
-    //   "clover"
-    // ],
+    coverageReporters: ["lcov"],
 
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: undefined,
