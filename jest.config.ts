3a4efa77d978fd2f8e4f7e83f509d535e2d2126f
npyl <pylarinosnick@gmail.com>
2025-08-26 17:52:57 +0300

    useVersioned: move into unit test1/2

--- jest.config.ts
+++ jest.config.ts
@@ -6,23 +6,11 @@
 import type { Config } from "jest";
 
 const config: Config = {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\nikos\\AppData\\Local\\Temp\\jest",
-
     // Automatically clear mock calls, instances, contexts and results before every test
     clearMocks: true,
 
     // Indicates whether the coverage information should be collected while executing the test
     collectCoverage: true,
-
-    // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: undefined,
 
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
@@ -150,52 +138,16 @@ const config: Config = {
     // The test environment that will be used for testing
     testEnvironment: "jsdom",
 
-    // Options that will be passed to the testEnvironment
-    // testEnvironmentOptions: {},
-
-    // Adds a location field to test results
-    // testLocationInResults: false,
-
-    // The glob patterns Jest uses to detect test files
-    // testMatch: [
-    //   "**/__tests__/**/*.?([mc])[jt]s?(x)",
-    //   "**/?(*.)+(spec|test).?([mc])[jt]s?(x)"
-    // ],
+    // Match **.test.(js|ts)(x)
+    testMatch: ["**/?(*.)test.?([mc])[jt]s?(x)"],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    // testPathIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
-    // ],
+    testPathIgnorePatterns: ["\\\\node_modules\\\\", "\\\\tests\\\\"],
 
-    // The regexp pattern or array of patterns that Jest uses to detect test files
-    // testRegex: [],
-
-    // This option allows the use of a custom results processor
-    // testResultsProcessor: undefined,
-
-    // This option allows use of a custom test runner
-    // testRunner: "jest-circus/runner",
-
-    // A map from regular expressions to paths to transformers
-    // transform: undefined,
-
-    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    // transformIgnorePatterns: [
-    //   "\\\\node_modules\\\\",
-    //   "\\.pnp\\.[^\\\\]+$"
-    // ],
-
-    // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
-    // unmockedModulePathPatterns: undefined,
-
-    // Indicates whether each individual test should be reported during the run
-    // verbose: undefined,
-
-    // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-    // watchPathIgnorePatterns: [],
-
-    // Whether to use watchman for file crawling
-    // watchman: true,
+    moduleNameMapper: {
+        "^@/test/(.*)$": "<rootDir>/test/$1",
+        "^@/(.*)$": "<rootDir>/src/$1",
+    },
 };
 
 export default config;
---
+++ src/hooks/useVersioned/__test__/index.comp.tsx
@@ -0,0 +1,76 @@
+import { FC, useCallback, useState } from "react";
+import useVersioned, { getVersioned, TVersioned } from "@/hooks/useVersioned";
+
+type TValue = {
+    an: string;
+    for: string;
+};
+
+const storeKey = "a-simple-key";
+
+const FALLBACK_VALUE: TValue = {
+    an: "object",
+    for: "reference",
+};
+const SPECIFIC_VALUE: TValue = {
+    an: "object2",
+    for: "reference2",
+};
+
+const VALUE_ID = "value-test-id";
+const SET_VALUE_ID = "set-test-id";
+const REMOVE_VALUE_ID = "remove-test-id";
+
+const VERSION_STORED = 1;
+
+/**
+ * Store w/ injected value support
+ */
+const useStore =
+    <T extends object>(injectedValue?: TVersioned<T>) =>
+    (_: string | null, fallbackValue: TVersioned<T>) => {
+        const [value, setState] = useState(injectedValue ?? fallbackValue);
+        const remove = useCallback(
+            () => setState(fallbackValue),
+            [fallbackValue]
+        );
+        return [value, setState, remove] as const;
+    };
+
+interface TesterProps {
+    injectedValue?: TVersioned<TValue>;
+    version?: number;
+}
+
+const Tester: FC<TesterProps> = ({
+    injectedValue,
+    version = VERSION_STORED,
+}) => {
+    const [value, set, remove] = useVersioned(
+        storeKey,
+        FALLBACK_VALUE,
+        version,
+        useStore(injectedValue)
+    );
+    const setValue = useCallback(() => set(SPECIFIC_VALUE), []);
+    return (
+        <div>
+            <button data-testid={SET_VALUE_ID} onClick={setValue} />
+            <button data-testid={REMOVE_VALUE_ID} onClick={remove} />
+            <div data-testid={VALUE_ID}>{JSON.stringify(value)}</div>
+        </div>
+    );
+};
+
+export {
+    VALUE_ID,
+    SET_VALUE_ID,
+    REMOVE_VALUE_ID,
+    // ...
+    FALLBACK_VALUE,
+    SPECIFIC_VALUE,
+    VERSION_STORED,
+    // ...
+    getVersioned,
+};
+export default Tester;
