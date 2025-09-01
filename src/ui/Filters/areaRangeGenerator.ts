17e5cfc36c959e4e0cbcfbd7c1dd1a10acc0b848
npyl <pylarinosnick@gmail.com>
2025-09-01 16:00:06 +0300

    SONAR: https://sonarcloud.io/project/issues?impactSeverities=HIGH&issueStatuses=OPEN%2CCONFIRMED&id=npylarinos_estate-fe&open=AZhhe7WQAksRpZxBr_Iq

--- src/ui/Filters/areaRangeGenerator.ts
+++ src/ui/Filters/areaRangeGenerator.ts
@@ -17,48 +17,24 @@ const STEP_TEN_K = 10000;
 
 // ---------------------------------------------------------------------------------------------
 
+// Configuration: [start, end, step]
 const GENERATOR = () => {
-    const numbers = [];
+    const numbers: number[] = [];
 
-    // 20 - 200 με βήμα 5
-    for (let i = TWENTY; i <= TWO_HUNDRED; i += STEP_FIVE) {
-        numbers.push(i);
-    }
+    const ranges = [
+        [TWENTY, TWO_HUNDRED, STEP_FIVE],
+        [TWO_HUNDRED + STEP_TEN, FIVE_HUNDRED, STEP_TEN],
+        [FIVE_HUNDRED + STEP_FIFTY, ONE_K, STEP_FIFTY],
+        [ONE_K + STEP_HUNDRED, TEN_K, STEP_HUNDRED],
+        [TEN_K + STEP_ONE_K, FIFTY_K, STEP_ONE_K],
+        [FIFTY_K + STEP_TEN_K, TWO_HUNDRED_K, STEP_TEN_K],
+    ];
 
-    // 200 - 500 με βήμα 10
-    for (let i = TWO_HUNDRED; i <= FIVE_HUNDRED; i += STEP_TEN) {
-        if (!numbers.includes(i)) {
+    ranges.forEach(([start, end, step]) => {
+        for (let i = start; i <= end; i += step) {
             numbers.push(i);
         }
-    }
-
-    // 500 - 1.000 με βημα 50
-    for (let i = FIVE_HUNDRED; i <= ONE_K; i += STEP_FIFTY) {
-        if (!numbers.includes(i)) {
-            numbers.push(i);
-        }
-    }
-
-    // 1.000 - 10.000 με βήμα 100
-    for (let i = ONE_K; i <= TEN_K; i += STEP_HUNDRED) {
-        if (!numbers.includes(i)) {
-            numbers.push(i);
-        }
-    }
-
-    // 10.000 - 50.000 με βήμα 1.000
-    for (let i = TEN_K; i <= FIFTY_K; i += STEP_ONE_K) {
-        if (!numbers.includes(i)) {
-            numbers.push(i);
-        }
-    }
-
-    // 50.000 - 200.000 με βήμα 10.000
-    for (let i = FIFTY_K; i <= TWO_HUNDRED_K; i += STEP_TEN_K) {
-        if (!numbers.includes(i)) {
-            numbers.push(i);
-        }
-    }
+    });
 
     return numbers;
 };
