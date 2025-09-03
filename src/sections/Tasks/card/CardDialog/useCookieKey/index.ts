6bfdb6bcdc8c6dbf912c82be81802a33eca4a598
npyl <npylarinos@digipath.gr>
2025-09-03 15:16:33 +0300

    useCookieKey: coverage.

--- src/sections/Tasks/card/CardDialog/useCookieKey/index.ts
+++ src/sections/Tasks/card/CardDialog/useCookieKey/index.ts
@@ -2,13 +2,16 @@ import useFormPersistStorageKey from "@/ui/useFormPersistStorageKey";
 import toNumberSafe from "@/utils/toNumberSafe";
 import { useRouter } from "next/router";
 
+const BASENAME = "PPTaskForm";
+
 const useCookieKey = (quickCreate: boolean) => {
     const router = useRouter();
     const { taskId } = router.query;
 
     const iTaskId = toNumberSafe(taskId);
 
-    return useFormPersistStorageKey("PPTaskForm", iTaskId, quickCreate);
+    return useFormPersistStorageKey(BASENAME, iTaskId, quickCreate);
 };
 
+export { BASENAME };
 export default useCookieKey;
