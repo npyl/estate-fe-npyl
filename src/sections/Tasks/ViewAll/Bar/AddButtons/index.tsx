d7f7f6a6a676590648f78b93255a416642776d56
npyl <npylarinos@digipath.gr>
2025-09-17 10:53:50 +0300

    unify ui.

--- src/sections/Tasks/ViewAll/Bar/AddButtons/index.tsx
+++ src/sections/Tasks/ViewAll/Bar/AddButtons/index.tsx
@@ -1,14 +1,10 @@
-import AddIcon from "@mui/icons-material/Add";
 import useDialog from "@/hooks/useDialog";
 import dynamic from "next/dynamic";
-import { useTranslation } from "react-i18next";
-import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import { IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { FC, useRef } from "react";
-import { HideText } from "@/components/styled";
-import { TASK } from "@/constants/tests";
+import CreateFab from "@/ui/CreateFab";
 const CardDialog = dynamic(() => import("@/sections/Tasks/card/CardDialog"));
 const Menu = dynamic(() => import("./Menu"));
 
@@ -17,8 +13,6 @@ interface AddButtonsProps {
 }
 
 const AddButtons: FC<AddButtonsProps> = ({ create = false }) => {
-    const { t } = useTranslation();
-
     const anchorRef = useRef(null);
 
     const [isMenuOpen, openMenu, closeMenu] = useDialog();
@@ -26,18 +20,7 @@ const AddButtons: FC<AddButtonsProps> = ({ create = false }) => {
 
     return (
         <Stack direction="row" alignItems="center" spacing={1} mt={1}>
-            <Button
-                data-testid={TASK.CREATE_ID}
-                variant="contained"
-                startIcon={<AddIcon />}
-                onClick={openDialog}
-                sx={{
-                    textWrap: "nowrap",
-                    ...HideText,
-                }}
-            >
-                {t("Add Task")}
-            </Button>
+            <CreateFab onClick={openDialog} />
 
             <IconButton ref={anchorRef} onClick={openMenu}>
                 <MenuIcon />
