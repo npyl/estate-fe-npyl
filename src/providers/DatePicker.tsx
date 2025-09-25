d3e8018413dffe28b5b34a74dddc6d21f228fda9
npyl <pylarinosnick@gmail.com>
2025-09-25 13:17:34 +0300

    localisation for datepicker.

--- src/providers/DatePicker.tsx
+++ src/providers/DatePicker.tsx
@@ -2,16 +2,41 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { FC, PropsWithChildren } from "react";
 import { EUROPEAN_DATE_FORMAT } from "@/constants/datepicker";
+import { enUS, elGR } from "@mui/x-date-pickers/locales";
+import { useTranslation } from "react-i18next";
 
-const DatePickerProvider: FC<PropsWithChildren> = ({ children }) => (
-    <LocalizationProvider
-        dateAdapter={AdapterDayjs}
-        dateFormats={{
-            keyboardDate: EUROPEAN_DATE_FORMAT,
-        }}
-    >
-        {children}
-    </LocalizationProvider>
-);
+// INFO: these are important
+import "dayjs/locale/en-gb";
+import "dayjs/locale/el";
+
+const LOCALE_MAP: Record<string, any> = {
+    en: {
+        adapterLocale: "en",
+        localeText:
+            enUS.components.MuiLocalizationProvider.defaultProps.localeText,
+    },
+    el: {
+        adapterLocale: "el",
+        localeText:
+            elGR.components.MuiLocalizationProvider.defaultProps.localeText,
+    },
+};
+
+const DatePickerProvider: FC<PropsWithChildren> = ({ children }) => {
+    const { i18n } = useTranslation();
+
+    const props = LOCALE_MAP[i18n.language];
+
+    return (
+        <LocalizationProvider
+            dateAdapter={AdapterDayjs}
+            dateFormats={{ keyboardDate: EUROPEAN_DATE_FORMAT }}
+            // ...
+            {...props}
+        >
+            {children}
+        </LocalizationProvider>
+    );
+};
 
 export default DatePickerProvider;
