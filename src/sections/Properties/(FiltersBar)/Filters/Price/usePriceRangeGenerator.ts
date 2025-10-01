920b5148a020344d7d1e79f194a39d16151b0b00
npyl <npylarinos@digipath.gr>
2025-10-01 15:34:40 +0300

    PP-210: Sonar; SALE by default

--- src/sections/Properties/(FiltersBar)/Filters/Price/usePriceRangeGenerator.ts
+++ src/sections/Properties/(FiltersBar)/Filters/Price/usePriceRangeGenerator.ts
@@ -2,24 +2,28 @@ import generatePriceRange from "@/ui/Filters/priceRangeGenerator";
 import { useStates } from "@/sections/Properties/FiltersContext";
 import { useCallback } from "react";
 
+// (RENT & RENTED) or (RENT or RENTED) or nothing -> 2, 1, 0 lengths, respectively
+const getRentState = (states: string[]) => {
+    const isRent = states.includes("RENT");
+    const isRented = states.includes("RENTED");
+
+    if (isRent && isRented) return 2;
+    if (isRent || isRented) return 1;
+    return 0;
+};
+
 const usePriceRangeGenerator = () => {
     const states = useStates();
 
-    // (RENT & RENTED) or (RENT or RENTED) or nothing -> 2, 1, 0 lengths, respectively
-    const rentLength =
-        states.includes("RENT") && states.includes("RENTED")
-            ? 2
-            : states.includes("RENT") || states.includes("RENTED")
-              ? 1
-              : 0;
+    const rentLength = getRentState(states);
 
     const isRent = rentLength > 0;
     const isSale = states.length > rentLength;
-    const nothingSelected = states.length === 0;
+    const isNothing = states.length === 0;
 
     const generateNumbers = useCallback(
-        () => generatePriceRange(nothingSelected, isRent, isSale),
-        [nothingSelected, isRent, isSale]
+        () => generatePriceRange(isNothing, isRent, isSale),
+        [isNothing, isRent, isSale]
     );
 
     return { generateNumbers };
