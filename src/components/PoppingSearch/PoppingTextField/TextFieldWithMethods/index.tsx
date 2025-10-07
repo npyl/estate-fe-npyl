e6c8a8c951aaea1ee29dd49d79aeba9ec9ea97ba
npyl <pylarinosnick@gmail.com>
2025-10-07 11:07:18 +0300

    Codesplit

---
+++ src/components/PoppingSearch/PoppingTextField/TextFieldWithMethods/index.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent, forwardRef, useCallback } from "react";
+import AdornmentLeft from "./AdornmentLeft";
+import AdornmentRight from "./AdornmentRight";
+import StyledTextField, { StyledSearchProps } from "./StyledTextField";
+
+interface TextFieldWithMethodsProps
+    extends Omit<StyledSearchProps, "onClick" | "onChange"> {
+    onClear: VoidFunction;
+    onChange: (s: string) => void;
+
+    onOpen: VoidFunction;
+    onClose: VoidFunction;
+}
+
+const TextFieldWithMethods = forwardRef<
+    HTMLInputElement,
+    TextFieldWithMethodsProps
+>(
+    (
+        { onClear, onChange: _onChange, onOpen, onClose, InputProps, ...props },
+        ref
+    ) => {
+        const onChange = useCallback(
+            (e: ChangeEvent<HTMLInputElement>) => _onChange?.(e.target.value),
+            [_onChange]
+        );
+
+        return (
+            <StyledTextField
+                ref={ref}
+                InputProps={{
+                    ...InputProps,
+                    startAdornment: <AdornmentLeft />,
+                    endAdornment: props.open ? (
+                        <AdornmentRight onClear={onClear} onClose={onClose} />
+                    ) : null,
+                }}
+                onClick={onOpen}
+                onChange={onChange}
+                {...props}
+            />
+        );
+    }
+);
+
+TextFieldWithMethods.displayName = "TextFieldWithMethods";
+
+export type { TextFieldWithMethodsProps };
+export default TextFieldWithMethods;
