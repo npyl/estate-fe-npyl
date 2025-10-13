61c8ea3dac141e4fff90e199f0b0bc0e4c507293
npyl <npylarinos@digipath.gr>
2025-10-13 14:37:34 +0300

    FIx big regression.

--- src/components/Calendar/Event/index.tsx
+++ src/components/Calendar/Event/index.tsx
@@ -14,7 +14,6 @@ const CalendarEvent = forwardRef<HTMLDivElement, CalendarEventProps>(
     (
         {
             event,
-            overlapCount = 0,
             onEventClick,
             // ...
             ...props
