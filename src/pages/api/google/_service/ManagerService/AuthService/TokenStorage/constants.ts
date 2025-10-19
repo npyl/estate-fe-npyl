a3b29930af6f06237a6c7a9aecada0ad3c47bc0f
npyl <npylarinos@digipath.gr>
2025-10-19 19:26:48 +0300

    .

--- src/pages/api/google/_service/ManagerService/AuthService/TokenStorage/constants.ts
+++ src/pages/api/google/_service/ManagerService/AuthService/TokenStorage/constants.ts
@@ -2,10 +2,6 @@ import path from "node:path";
 
 const STORAGE_DIR = ".pp-persistent";
 
-const TOKEN_FILE_PATH = path.join(
-    process.cwd(),
-    STORAGE_DIR,
-    "refresh_tokens.json"
-);
+const TOKEN_FILE_PATH = path.join(process.cwd(), STORAGE_DIR, "tokens.json");
 
 export { TOKEN_FILE_PATH };
