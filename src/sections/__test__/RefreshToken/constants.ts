35e3aac656658407efef063202b38b5ae9dd0351
npyl <pylarinosnick@gmail.com>
2025-10-23 12:52:16 +0300

    Absolute URLs

--- src/sections/__test__/RefreshToken/constants.ts
+++ src/sections/__test__/RefreshToken/constants.ts
@@ -1,9 +1,9 @@
 const OPEN_VIEW_BUTTON_TESTID = "open-view-button-testid";
 const HIDDEN_VIEW_TESTID = "hidden-view-testid";
 
-const URL0 = "/playwright/URL0";
-const URL1 = "/playwright/URL1";
-const URL2 = "/playwright/URL2";
+const URL0 = "http://something.com/URL0";
+const URL1 = "http://something.com/URL1";
+const URL2 = "http://something.com/URL2";
 
 export {
     OPEN_VIEW_BUTTON_TESTID,
